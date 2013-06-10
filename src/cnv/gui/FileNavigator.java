c68562eae1a30ec00d88c7677e225edf59a908f6
Michael Vieths <mvieths@gmail.com>
2013-06-09 20:31:56 -0500

    Color code rectangles by file
    Color code rectangles by file

--- src/cnv/gui/FileNavigator.java
+++ src/cnv/gui/FileNavigator.java
@@ -1,5 +1,7 @@
 package cnv.gui;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,7 +9,10 @@ import java.awt.event.ActionListener;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
 
 import common.Grafik;
 
@@ -21,7 +26,7 @@ public class FileNavigator extends JPanel implements ActionListener {
 	/**
 	 * Create the panel.
 	 */
-	public FileNavigator(String[] files) {
+	public FileNavigator(String[] files, Color[] colors) {
 
 		JButton leftButton = new JButton(Grafik.getImageIcon("images/firstLast/dLeft.gif", true));
 		add(leftButton);
@@ -30,6 +35,13 @@ public class FileNavigator extends JPanel implements ActionListener {
 		comboBox.setModel(new DefaultComboBoxModel<String>(files));
 		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		add(comboBox);
+
+		ComboBoxRenderer renderer = new ComboBoxRenderer(comboBox);
+
+		renderer.setColors(colors);
+		renderer.setStrings(files);
+
+		comboBox.setRenderer(renderer);
 
 		JButton rightButton = new JButton(Grafik.getImageIcon("images/firstLast/dRight.gif", true));
 		add(rightButton);
@@ -40,5 +52,60 @@ public class FileNavigator extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 
+	}
+}
+
+class ComboBoxRenderer extends JPanel implements ListCellRenderer<Object> {
+
+	private static final long serialVersionUID = -1L;
+	private Color[] colors;
+	private String[] strings;
+
+	JPanel textPanel;
+	JLabel text;
+
+	public ComboBoxRenderer(JComboBox<String> combo) {
+
+		textPanel = new JPanel();
+		textPanel.add(this);
+		text = new JLabel();
+		text.setOpaque(true);
+		text.setFont(combo.getFont());
+		textPanel.add(text);
+	}
+
+	public void setColors(Color[] col) {
+		colors = col;
+	}
+
+	public void setStrings(String[] str) {
+		strings = str;
+	}
+
+	public Color[] getColors() {
+		return colors;
+	}
+
+	public String[] getStrings() {
+		return strings;
+	}
+
+	@Override
+	public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+
+		if (isSelected) {
+			setBackground(list.getSelectionBackground());
+		} else {
+			setBackground(Color.WHITE);
+		}
+
+		text.setBackground(getBackground());
+
+		// Wrap around if we have more files than colors
+		text.setText(value.toString());
+		if (index > -1) {
+			text.setForeground(colors[colors.hashCode() % (index + 1)]);
+		}
+		return text;
 	}
 }
