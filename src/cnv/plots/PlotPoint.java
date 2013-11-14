d55f151d93c1dd26e80acda7d16352429afb1115
Rohit Sinha <talktorohit54@gmail.com>
2013-11-14 04:01:39 -0600

    Added copy constructor to PlotPoint

--- src/cnv/plots/PlotPoint.java
+++ src/cnv/plots/PlotPoint.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class PlotPoint implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final byte FILLED_CIRCLE = 1;
 	public static final byte OPEN_CIRCLE = 2;
 	public static final byte MISSING = 3;
@@ -13,7 +13,7 @@ public class PlotPoint implements Serializable {
 	public static final byte FILLED_TRIANGLE = 6;
 	public static final String MISSING_STR = "X";
 	public static final String NAN_STR = "NaN";
-	
+
 	private String id;
 	private byte type;
 	private float rawX;
@@ -34,6 +34,24 @@ public class PlotPoint implements Serializable {
 		this.layer = layer;
 		this.highlight = false;
 		this.visible = true;
+	}
+
+	/**
+	 * Copy constructor. Don't depend on clone() to copy objects. Its messy!!!
+	 * 
+	 * @param point
+	 *            a {@link PlotPoint} which is the source of copy
+	 */
+	public PlotPoint(PlotPoint point) {
+		this.id = point.id;
+		this.type = point.type;
+		this.rawX = point.rawX;
+		this.rawY = point.rawY;
+		this.size = point.size;
+		this.color = point.color;
+		this.layer = point.layer;
+		this.highlight = point.highlight;
+		this.visible = point.visible;
 	}
 
 	public String getId() {
