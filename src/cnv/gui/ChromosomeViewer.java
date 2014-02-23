37c33710d0d047b26d5d0cc029b68aeb6ac959b6
Michael Vieths <mvieths@gmail.com>
2014-02-23 15:29:25 -0600

    Fix parallax issue in chromosome viewer
    X coordinate was being incorrectly calculated, causing chromosome
    drawing to shift on scrolling
    Commented out a debug message in CNVRectangles

--- src/cnv/gui/ChromosomeViewer.java
+++ src/cnv/gui/ChromosomeViewer.java
@@ -13,114 +13,114 @@ import filesys.GeneTrack;
 import filesys.Segment;
 
 public class ChromosomeViewer extends JPanel {
-    public static final long serialVersionUID = 8L;
+	public static final long serialVersionUID = 8L;
 
-    public static final int  WIDTH_BUFFER     = 25;
+	public static final int WIDTH_BUFFER = 25;
 
-    private int              chromosome       = 1;
-    private int              startPosition    = 0;
-    private int              stopPosition     = 0;
-    private GeneTrack        track            = null; // Represents where the genes are
-    float                    scalingFactor;
+	private int chromosome = 1;
+	private int startPosition = 0;
+	private int stopPosition = 0;
+	private GeneTrack track = null; // Represents where the genes are
+	float scalingFactor;
 
-    public ChromosomeViewer(int chr, int start, int stop, GeneTrack gt) {
-        chromosome = chr;
-        startPosition = start;
-        stopPosition = stop;
-        track = gt;
+	public ChromosomeViewer(int chr, int start, int stop, GeneTrack gt) {
+		chromosome = chr;
+		startPosition = start;
+		stopPosition = stop;
+		track = gt;
 
-        setMaximumSize(new Dimension(getWidth(), 20));
-        setMinimumSize(new Dimension(getWidth(), 20));
-    }
+		setMaximumSize(new Dimension(getWidth(), 20));
+		setMinimumSize(new Dimension(getWidth(), 20));
+	}
 
-    public void setChr(int chr) {
-        chromosome = chr;
-    }
+	public void setChr(int chr) {
+		chromosome = chr;
+	}
 
-    public void setStart(int start) {
-        startPosition = start;
-    }
+	public void setStart(int start) {
+		startPosition = start;
+	}
 
-    public void setStop(int stop) {
-        stopPosition = stop;
-    }
+	public void setStop(int stop) {
+		stopPosition = stop;
+	}
 
-    public void setTrack(GeneTrack track) {
-        this.track = track;
-    }
+	public void setTrack(GeneTrack track) {
+		this.track = track;
+	}
 
-    public void updateView(int chr, int start, int stop) {
-        chromosome = chr;
-        startPosition = start;
-        stopPosition = stop;
+	public void updateView(int chr, int start, int stop) {
+		chromosome = chr;
+		startPosition = start;
+		stopPosition = stop;
 
-        int window = stopPosition - startPosition;
-        scalingFactor = (float) getWidth() / window;
-        // scalingFactor = 1;
+		int window = stopPosition - startPosition;
+		scalingFactor = (float) getWidth() / window;
+		// scalingFactor = 1;
 
-        repaint();
-    }
+		repaint();
+	}
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        GeneData[] genes;
-        int[][] exons;
-        Vector<Segment> v = new Vector<Segment>();
-        Segment[] segs;
-        int width, begin, end;
-        int height = 10;
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		GeneData[] genes;
+		int[][] exons;
+		Vector<Segment> v = new Vector<Segment>();
+		Segment[] segs;
+		int width, begin, end;
+		int height = 10;
 
-        // System.out.println("Start = " + startPosition + "  Stop = " + stopPosition + " Difference is " + (stopPosition - startPosition));
-        if (track == null) {
-            System.out.println("Track is null");
-        }
+		// System.out.println("Start = " + startPosition + "  Stop = " + stopPosition + " Difference is " + (stopPosition - startPosition));
+		if (track == null) {
+			System.out.println("Track is null");
+		}
 
-        try {
-            if (track != null && stopPosition - startPosition < 10000000) {
-                genes = track.getBetween(chromosome, startPosition, stopPosition, 30);
-                g.setColor(Color.BLACK);
-                for (int i = 0; i < genes.length; i++) {
-                    // begin = (int) (getX(genes[i].getStart()) * scalingFactor);
-                    // end = (int) (getX(genes[i].getStop()) * scalingFactor);
-                    begin = (int) (getX(genes[i].getStart()) * scalingFactor);
-                    end = (int) (getX(genes[i].getStop()) * scalingFactor);
-                    width = end - begin;
-                    if (width < 1) {
-                        width = 1;
-                    }
-                    g.drawRoundRect(begin, 0, width, height, 2, 2);
-                    v.add(new Segment(begin, end));
-                    exons = genes[i].getExonBoundaries();
-                    for (int j = 0; j < exons.length; j++) {
-                        begin = getX(exons[j][0]);
-                        end = getX(exons[j][1]);
-                        if (j == 0 || j == exons.length - 1) {
-                            g.fillRoundRect(begin, 0, width, height, 2, 2);
-                        } else {
-                            g.fillRect(begin, 0, width, height);
-                        }
+		try {
+			if (track != null && stopPosition - startPosition < 10000000) {
+				genes = track.getBetween(chromosome, startPosition, stopPosition, 30);
+				g.setColor(Color.BLACK);
+				for (int i = 0; i < genes.length; i++) {
+					begin = getX(genes[i].getStart());
+					end = getX(genes[i].getStop());
+					width = end - begin;
+					if (width < 1) {
+						width = 1;
+					}
+					g.drawRoundRect(begin, 0, width, height, 2, 2);
+					v.add(new Segment(begin, end));
+					exons = genes[i].getExonBoundaries();
+					for (int j = 0; j < exons.length; j++) {
 
-                    }
-                }
-                Segment.mergeOverlapsAndSort(v);
-                segs = Segment.toArray(v);
-                g.setFont(new Font("Arial", 0, 14));
+						int exBegin = getX(exons[j][0]);
+						int exEnd = getX(exons[j][1]);
+						int exWidth = exEnd - exBegin;
+						if (j == 0 || j == exons.length - 1) {
+							g.fillRoundRect(exBegin, 0, exWidth, height, 2, 2);
+						} else {
+							g.fillRect(exBegin, 0, exWidth, height);
+						}
 
-                for (int i = 0; i < genes.length; i++) {
-                    begin = getX(genes[i].getStart());
-                    width = g.getFontMetrics(g.getFont()).stringWidth(genes[i].getGeneName());
-                    if (!Segment.overlapsAny(new Segment(begin - width - 5, begin - 1), segs)) {
-                        g.drawString(genes[i].getGeneName(), begin, (height * 2) + 2);
-                    }
-                }
+					}
+				}
+				Segment.mergeOverlapsAndSort(v);
+				segs = Segment.toArray(v);
+				g.setFont(new Font("Arial", 0, 14));
 
-            }
-        } catch (Exception ex) {
-            System.out.println("Missing data in gene track");
-        }
-    }
+				for (int i = 0; i < genes.length; i++) {
+					begin = getX(genes[i].getStart());
+					width = g.getFontMetrics(g.getFont()).stringWidth(genes[i].getGeneName());
+					if (!Segment.overlapsAny(new Segment(begin - width - 5, begin - 1), segs)) {
+						g.drawString(genes[i].getGeneName(), begin, (height * 2) + 2);
+					}
+				}
 
-    public int getX(int pos) {
-        return (int) ((double) (pos - startPosition) / (double) (stopPosition - startPosition) * (double) (getWidth() - 2 * WIDTH_BUFFER)) + WIDTH_BUFFER;
-    }
+			}
+		} catch (Exception ex) {
+			System.out.println("Missing data in gene track");
+		}
+	}
+
+	public int getX(int pos) {
+		return (int) ((pos - startPosition) * scalingFactor);
+	}
 }
