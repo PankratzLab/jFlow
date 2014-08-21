662597e4520dcdb55fdb7782c7f5d1ca1cdc66bc
Nathan Pankratz <pankr018@umn.edu>
2014-08-21 12:09:52 -0500

    SeattleSeq prepwork for SuperNovo
    PrincipalComponentsCompute - more descriptive log
    
    MitoPipeline - sleeps for 5 secs, in an attempt to avoid SampleList err
    
    SeattleSeq - cleanup of code, updated BAD list, added
    	loadAllAnnotationInDir()

--- src/bioinformatics/SeattleSeq.java
+++ src/bioinformatics/SeattleSeq.java
@@ -10,7 +10,8 @@ public class SeattleSeq {
 	public static final String[][] RELEVANTS = {{"chromosome"}, {"position"}, {"sampleAlleles"}, {"accession"}, {"functionGVS"}, {"aminoAcids"}, {"geneList"}, {"inDBSNPOrNot"}, {"rsID"}, {"microRNAs"}};
 //	"# inDBSNPOrNot", "", "position", "referenceBase", "sampleGenotype", "sampleAlleles", "allelesDBSNP", "accession", "functionGVS", "functionDBSNP", "rsID", "aminoAcids", "proteinPosition", "cDNAPosition", "polyPhen", "granthamScore", "scorePhastCons", "consScoreGERP", "chimpAllele", "CNV", "geneList", "AfricanHapMapFreq", "EuropeanHapMapFreq", "AsianHapMapFreq", "hasGenotypes", "dbSNPValidation", "repeatMasker", "tandemRepeat", "clinicalAssociation", "distanceToSplice", "microRNAs", "proteinSequence"
 	public static final String[] ORDER = {"stop-gained-near-splice", "stop-gained", "stop-lost-near-splice", "stop-lost", "nonsense", "missense-near-splice", "missense", "coding", "splice-donor", "splice-5", "splice-acceptor", "splice-3", "synonymous-near-splice", "intron-near-splice", "coding-unknown-near-splice", "synonymous", "coding-synonymous", "coding-unknown", "coding-notMod3", "non-coding-exon-near-splice", "non-coding-exon", "5-prime-UTR", "utr-5", "3-prime-UTR", "utr-3", "intron", "upstream-gene", "near-gene-5", "downstream-gene", "near-gene-3", "intergenic"};
-	public static final String[] BAD = {"missense", "stop-gained", "stop-lost", "missense-near-splice", "splice-donor", "splice-acceptor"};
+//	public static final String[] BAD = {"missense", "stop-gained", "stop-lost", "missense-near-splice", "splice-donor", "splice-acceptor"};
+	public static final String[] BAD = {"stop-gained-near-splice", "stop-gained", "stop-lost-near-splice", "stop-lost", "nonsense", "missense-near-splice", "missense", "coding", "splice-donor", "splice-5", "splice-acceptor", "splice-3"};
 	public static final String[] NEUTRAL = {"intron-near-splice", "5-prime-UTR", "downstream-gene", "upstream-gene", "synonymous", "coding-synonymous", "intergenic", "non-coding-exon", "3-prime-UTR", "intron", "coding-notMod3"};
 	
 	public static void proc(String filename) {
@@ -60,7 +61,6 @@ public class SeattleSeq {
 			System.err.println("Error reading file \"" + filename + "\"");
 			System.exit(2);
 		}
-		
 	}
 	
 	public static char[] determineAlleles(String str) {
@@ -77,102 +77,6 @@ public class SeattleSeq {
 		line = str.split("/");
 		
 		return new char[] {line[0].charAt(0), line[1].charAt(0)};		
-	}
-	
-	// the indels are not captured by SeattleSeq
-	// there are triallelic markers on the exome chip, hence then Hashtable, but that doesn't necessarily caputre all, still missing a little over a hundred
-	public static void summarize(String filename) {
-		BufferedReader reader;
-		PrintWriter writer;
-		String[] line, trav;
-		String temp;
-		Hashtable<String, Vector<String[]>> hash = new Hashtable<String, Vector<String[]>>();
-		Vector<String[]> v;
-		int[] indices;
-		Logger log;
-		String prev;
-		boolean done;
-		int type, worstType, worst;
-		String[] keys;
-		Hashtable<String,String> hashFreq;
-		double freq;
-		
-		hashFreq = HashVec.loadFileToHashString("D:/home/npankrat/NCBI/ESP_exome_chip/EVS/freqInfo_proc.dat", new int[] {0}, new int[] {3}, false, "", true, false, false);
-		
-		try {
-			reader = new BufferedReader(new FileReader(filename));
-			writer = new PrintWriter(new FileWriter(ext.rootOf(filename, false)+"_summary.out"));
-			writer.println(Array.toStr(Matrix.extractColumn(RELEVANTS, 0)));
-			log = new Logger(ext.rootOf(filename, false)+".log");
-			temp = reader.readLine().trim();
-			if (temp.startsWith("#"));
-			temp = temp.substring(1).trim();
-			line = temp.split("[\\s]+");
-			indices = ext.indexFactors(RELEVANTS, line, false, true, true, log, true);
-//			public static final String[][] RELEVANTS = {{"chromosome"}, {"position"}, {"accession"}, {"functionGVS"}, {"aminoAcids"}, {"geneList"}, {"inDBSNPOrNot"}, {"microRNAs"}};
-			
-			prev = "";
-			
-			done = false;
-			while (!done) {
-				if (reader.ready()) {
-					line = reader.readLine().trim().split("[\\s]+");
-				} else {
-					done = true;
-				}
-				if (hash.size() > 0 && !(line[indices[0]]+"\t"+line[indices[1]]).equals(prev) || done) {
-					keys = HashVec.getKeys(hash);
-					for (int key = 0; key < keys.length; key++) {
-						v = hash.get(keys[key]);
-						worst = -1;
-						worstType = ORDER.length;
-						type = -1;
-						for (int i = 0; i < v.size(); i++) {
-							trav = v.elementAt(i);
-							type = ext.indexOfStr(trav[indices[4]], ORDER);
-							if (type == -1) {
-								System.out.println("unknown type: "+trav[indices[4]]);
-								System.out.println(Array.toStr(trav));
-								System.exit(1);
-							}
-							if (type < worstType) {
-								worstType = type;
-								worst = i;
-							}						
-						}
-						trav = v.elementAt(worst);
-						if (worstType == 0) {
-							if (trav[indices[5]].startsWith("stop")) {
-								trav[indices[4]] = "stoploss";
-							} else {
-								trav[indices[4]] = "stopgain";
-							}
-						}
-						for (int i = 0; i < indices.length; i++) {
-							writer.print((i==0?"":"\t")+trav[indices[i]]);
-						}
-						if (hashFreq.containsKey(line[indices[0]].toLowerCase()+"^"+line[indices[1]])) {
-							freq = Double.parseDouble(hashFreq.get(line[indices[0]].toLowerCase()+"^"+line[indices[1]]));
-							writer.print("\t"+freq+"\t"+(freq<0.01?1:0)+"\t"+(freq<0.05?1:0));
-						} else {
-							writer.print("\t.\t.\t.");
-						}
-						writer.println();
-					}
-					hash = new Hashtable<String, Vector<String[]>>();
-				}
-				HashVec.addToHashArrayVec(hash, line[indices[2]], line);
-				prev = line[indices[0]]+"\t"+line[indices[1]];
-			}
-			writer.close();
-			reader.close();
-		} catch (FileNotFoundException fnfe) {
-			System.err.println("Error: file \"" + filename + "\" not found in current directory");
-			System.exit(1);
-		} catch (IOException ioe) {
-			System.err.println("Error reading file \"" + filename + "\"");
-			System.exit(2);
-		}
 	}
 	
 	public static void parseFreq(String filename) {
@@ -210,7 +114,9 @@ public class SeattleSeq {
 		}
 	}
 
-	public static void geneCounts(String filename, String freqFilename) {
+	// the indels are not captured by SeattleSeq
+	// there are triallelic markers on the exome chip, hence the Hashtable, but that doesn't necessarily capture all, still missing a little over a hundred
+	public static void summarize(String filename, String freqFilename) {
 		BufferedReader reader;
 		PrintWriter writer;
 		String[] line, trav;
@@ -235,11 +141,10 @@ public class SeattleSeq {
 			writer.println(Array.toStr(Matrix.extractColumn(RELEVANTS, 0))+"\tAlleleFrequency\tMAF<1%\tMAF<5%");
 			log = new Logger(ext.rootOf(filename, false)+".log");
 			temp = reader.readLine().trim();
-			if (temp.startsWith("#"));
+//			if (temp.startsWith("#"));
 			temp = temp.substring(1).trim();
 			line = temp.split("[\\s]+");
 			indices = ext.indexFactors(RELEVANTS, line, false, true, true, log, true);
-//			public static final String[][] RELEVANTS = {{"chromosome"}, {"position"}, {"accession"}, {"functionGVS"}, {"aminoAcids"}, {"geneList"}, {"inDBSNPOrNot"}, {"microRNAs"}};
 			
 			prev = "";
 			
@@ -310,7 +215,7 @@ public class SeattleSeq {
 		}
 	}
 	
-	public static Hashtable<String, String[]> loadAllAnnotationInDir(String directory, Logger log) {
+	public static Hashtable<String, String[]> loadAllBadAnnotationInDir(String directory, Logger log) {
 		BufferedReader reader;
 		String[] files, line;
 		Hashtable<String, String[]> hash;
@@ -365,21 +270,155 @@ public class SeattleSeq {
 		return hash;
 	}
 
+	public static Hashtable<String, String[]> loadAllAnnotationInDir(String directory, Logger log) {
+		BufferedReader reader;
+		String[] files, line;
+		Hashtable<String, String[]> finalHash;
+		String markerName;
+		String[] trav;
+		String temp;
+		Vector<String[]> v;
+		int[] indices;
+		String prev;
+		boolean done;
+		int type, worstType, worst;
+		int linesSkipped;
+		boolean problem;
+
+		problem = false;
+		finalHash = new Hashtable<String, String[]>();
+		
+		if (directory == null) {
+			log.reportError("The SeattleSeq annotation directory was null; returning an empty hashtable");
+		} else if (!Files.exists(directory) || !Files.isDirectory(directory)) {
+			log.reportError("Error - SeattleSeq annotation directory directory not found: "+directory);
+			log.reportError("        returning an empty hashtable");
+		} else {
+			files = Files.list(directory, "SeattleSeqAnnotation", ".txt.gz", false, false);
+			log.report("Found "+files.length+" file(s) with a .SeattleSeq extension to include");
+			for (int f = 0; f < files.length; f++) {
+				log.report("Processing SeattleSeq file "+(f+1)+" of "+files.length+"\t"+files[f], false, true);
+				try {
+					reader = Files.getAppropriateReader(directory+files[f]);
+
+					temp = reader.readLine().trim();
+					temp = temp.substring(1).trim();
+					line = temp.split("[\\s]+");
+					indices = ext.indexFactors(RELEVANTS, line, false, true, true, log, true);
+					
+					prev = "";
+					
+					done = false;
+					trav = null;
+					linesSkipped = 0;
+					v = new Vector<String[]>();
+					while (!done) {
+						if (reader.ready()) {
+							line = reader.readLine().trim().split("[\\s]+");
+							if (line[0].startsWith("#") || line.length < Array.max(indices)) {
+								linesSkipped++;
+								continue;
+							}
+							
+							try {
+								markerName = "chr"+line[1]+":"+line[2]+"_"+line[3]+"_"+line[4];
+								if (v.size() > 0 && !markerName.equals(prev) || done) {
+									worst = -1;
+									worstType = ORDER.length;
+									type = -1;
+									for (int i = 0; i < v.size(); i++) {
+										trav = v.elementAt(i);
+										type = ext.indexOfStr(trav[indices[4]], ORDER);
+										if (type == -1) {
+											log.reportError("unknown type: "+trav[indices[4]]);
+											log.reportError(Array.toStr(trav));
+											problem = true;
+										}
+										if (type < worstType) {
+											worstType = type;
+											worst = i;
+										}						
+									}
+									trav = v.elementAt(worst);
+									if (trav[indices[8]].equals("0")) {
+										trav[indices[8]] = ".";
+									} else {
+										trav[indices[8]] = "rs"+trav[indices[8]];
+									}
+									
+									trav = Array.subArray(trav, indices);	// trim to the relevant columns
+									trav = Array.addStrToArray(ext.indexOfStr(trav[4], BAD) >= 0?"1":"0", trav, 0); // determine if mutation is missense/splice or worse 
+									finalHash.put(markerName, trav);
+									v = new Vector<String[]>();
+								}
+								v.add(line);
+								prev = markerName;
+							} catch (Exception e) {
+								log.reportError("Error reading line: "+Array.toStr(line));
+								log.reportException(e);
+								problem = true;
+								System.exit(1);
+							}								
+						} else {
+							done = true;
+						}
+					}
+					
+					log.report(" ...skipped "+linesSkipped+" line(s)");
+					reader.close();
+				} catch (FileNotFoundException fnfe) {
+					System.err.println("Error: file \"" + directory+files[f]
+							+ "\" not found in current directory");
+					System.exit(1);
+				} catch (IOException ioe) {
+					System.err.println("Error reading file \"" + directory+files[f] + "\"");
+					System.exit(2);
+				}
+			}
+		}		
+
+		if (problem) {
+			return null;
+		} else {
+			return finalHash;
+		}
+	}
+
 	public static void main(String[] args) {
 		int numArgs = args.length;
 		String filename = "SeattleSeq.dat";
+		String prep = null;
+		String prepFreq = null; // "D:/home/npankrat/NCBI/ESP_exome_chip/EVS/freqInfo.out";
 		String freqFilename = "D:/home/npankrat/NCBI/ESP_exome_chip/EVS/freqInfo_proc.dat";
 
+		
+//		Hashtable<String, String[]> hash = loadAllAnnotationInDir("D:/Logan/SuperNovo/SeattleSeq/", new Logger());
+//		String[] keys = HashVec.getKeys(hash);
+//		for (int i = 0; i < keys.length; i++) {
+//			System.out.println(keys[i]+"\t"+Array.toStr(hash.get(keys[i])));
+//		}
+//		System.exit(1);
+		
 		String usage = "\n" + 
 		"bioinformatics.SeattleSeq requires 0-1 arguments\n" + 
 		"   (1) filename of SeattleSeq annotation (i.e. file=" + filename + " (default))\n" + 
 		"   (2) filename of allele frequency data (i.e. freqFile=" + freqFilename + " (default))\n" + 
+		" OR:\n" + 
+		"   (1) name of file to prep for input to SeattleSeq (i.e. prep=filename.txt (not the default))\n" + 
+		" OR:\n" + 
+		"   (1) name of allele freq file to process (i.e. prepFreq=freqInfo.out (not the default))\n" + 
 		"";
 
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("-h") || args[i].equals("-help") || args[i].equals("/h") || args[i].equals("/help")) {
 				System.err.println(usage);
 				System.exit(1);
+			} else if (args[i].startsWith("prep=")) {
+				prep = args[i].split("=")[1];
+				numArgs--;
+			} else if (args[i].startsWith("prepFreq=")) {
+				prepFreq = args[i].split("=")[1];
+				numArgs--;
 			} else if (args[i].startsWith("file=")) {
 				filename = args[i].split("=")[1];
 				numArgs--;
@@ -394,18 +433,15 @@ public class SeattleSeq {
 			System.err.println(usage);
 			System.exit(1);
 		}
-//		filename = "D:/home/npankrat/NCBI/ESP_exome_chip/seattleInput.txt";
-//		filename = "D:/home/npankrat/NCBI/ESP_exome_chip/SeattleSeqAnnotation131.seattleInput.input.240042724786.txt";
-//		filename = "D:/Logan/SuperNovo/SeattleSeq/SeattleSeqAnnotation138.input.307359078671.txt.gz";
-//		filename = "D:/Logan/SuperNovo/SeattleSeq/SeattleSeqAnnotation138.input_new.307436705306.txt.gz";
-		
-//		filename = "D:/home/npankrat/NCBI/ESP_exome_chip/FGG.txt";
-//		filename = "D:/home/npankrat/NCBI/ESP_exome_chip/F11.txt";
+
 		try {
-//			proc(filename);
-//			summarize(filename);
-			geneCounts(filename, freqFilename);
-//			parseFreq("D:/home/npankrat/NCBI/ESP_exome_chip/EVS/freqInfo.out");
+			if (prep != null) {
+				proc(filename);
+			} else if (prepFreq != null) {
+				parseFreq(prepFreq);
+			} else {
+				summarize(filename, freqFilename);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
