2f2db7795305d0e1d8587eef0e49572b8f3bfc7b
Ben <rcoleb@gmail.com>
2015-04-06 02:25:25 -0500

    Grafik: add method to scale CheckBox icon by Font size; Configurator: bug fixes, UI/UX fixes/perfections, add keyboard support/standardization

--- src/common/Grafik.java
+++ src/common/Grafik.java
@@ -1,13 +1,62 @@
 package common;
 
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.UIManager;
 
 public class Grafik {
+
+	public static void scaleCheckBoxIcon(JCheckBox checkbox){
+		// from http://stackoverflow.com/a/26995048/875496 
+	    boolean previousState = checkbox.isSelected();
+	    checkbox.setSelected(false);
+	    FontMetrics boxFontMetrics =  checkbox.getFontMetrics(checkbox.getFont());
+	    Icon boxIcon = UIManager.getIcon("CheckBox.icon");
+	    BufferedImage boxImage = new BufferedImage(
+	        boxIcon.getIconWidth(), boxIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB
+	    );
+	    Graphics graphics = boxImage.createGraphics();
+	    try{
+	        boxIcon.paintIcon(checkbox, graphics, 0, 0);
+	    }finally{
+	        graphics.dispose();
+	    }
+	    ImageIcon newBoxImage = new ImageIcon(boxImage);
+	    Image finalBoxImage = newBoxImage.getImage().getScaledInstance(
+	        boxFontMetrics.getHeight(), boxFontMetrics.getHeight(), Image.SCALE_SMOOTH
+	    );
+	    checkbox.setIcon(new ImageIcon(finalBoxImage));
+	
+	    checkbox.setSelected(true);
+	    Icon checkedBoxIcon = UIManager.getIcon("CheckBox.icon");
+	    BufferedImage checkedBoxImage = new BufferedImage(
+	        checkedBoxIcon.getIconWidth(),  checkedBoxIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB
+	    );
+	    Graphics checkedGraphics = checkedBoxImage.createGraphics();
+	    try{
+	        checkedBoxIcon.paintIcon(checkbox, checkedGraphics, 0, 0);
+	    }finally{
+	        checkedGraphics.dispose();
+	    }
+	    ImageIcon newCheckedBoxImage = new ImageIcon(checkedBoxImage);
+	    Image finalCheckedBoxImage = newCheckedBoxImage.getImage().getScaledInstance(
+	        boxFontMetrics.getHeight(), boxFontMetrics.getHeight(), Image.SCALE_SMOOTH
+	    );
+	    checkbox.setSelectedIcon(new ImageIcon(finalCheckedBoxImage));
+	    checkbox.setSelected(false);
+	    checkbox.setSelected(previousState);
+	}
+	
+	
 	public static void drawThickLine(Graphics g, int x1, int y1, int x2, int y2, int thickness, Color c) {
 		int dX = x2-x1;
 		int dY = y2-y1;
