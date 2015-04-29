326f05df5977e1e99bf48c3d4ba402672d5b3059
cole0482 <cole0482@umn.edu>
2015-04-29 15:41:03 -0500

    Fixes to AuthorCorral, feature add'n for RTF output

--- src/one/AuthorCorral.java
+++ src/one/AuthorCorral.java
@@ -12,6 +12,24 @@ import common.ext;
 
 public class AuthorCorral {
 	
+	private static String rtfFormatted(String str, boolean authStr) {
+		StringBuilder sb = new StringBuilder();
+		String PREFIX = "{\\rtlch\\fcs1 \\af31507 \\ltrch\\fcs0 ";
+		sb.append(PREFIX);
+		for (int i = 0; i < str.length(); i++) {
+			sb.append(str.charAt(i));
+		}
+		sb.append("}");
+		String ret = sb.toString();
+		if (authStr) {
+			ret = sb.toString().replaceAll("#", "}{\\\\rtlch\\\\fcs1 \\\\af0\\\\afs24 \\\\ltrch\\\\fcs0 \\\\f0\\\\fs24\\\\super\\\\insrsid6508612 \\\\hich\\\\af0\\\\dbch\\\\af31505\\\\loch\\\\f0 ");
+			ret = ret.replaceAll("_", "}{\\\\rtlch\\\\fcs1 \\\\af31507 \\\\ltrch\\\\fcs0 ");
+		}
+		return ret;
+	}
+	
+	static final String PARAGRAH = "{\\rtlch\\fcs1 \\af0\\afs24 \\ltrch\\fcs0 \\line }";
+	
 	/*
 		Tab-Delimited Authorship file:
 			[0] First
@@ -23,7 +41,7 @@ public class AuthorCorral {
 			[6] E-mail
 			[7+] contribution columns - title taken verbatim from column title
 	*/
-	public static void run(String inFile, String outFile) throws IOException {
+	public static void run(String inFile, String outFile, boolean rtfOutput, boolean printErr) throws IOException {
 		BufferedReader inReader = Files.getReader(inFile, false, true, true);
 		String[] header = inReader.readLine().split("\t");
 		
@@ -31,13 +49,13 @@ public class AuthorCorral {
 		ArrayList<String> contribOrder = new ArrayList<String>();
 		HashMap<String, String[]> fullnameToParts = new HashMap<String, String[]>();
 		HashMap<String, ArrayList<String[]>> namesToDepts = new HashMap<String, ArrayList<String[]>>();
-		HashMap<String, ArrayList<String>> contribsToInitials = new HashMap<String, ArrayList<String>>();
+		HashMap<String, ArrayList<String>> contribsToNames = new HashMap<String, ArrayList<String>>();
 		
 		ArrayList<String> errors = new ArrayList<String>();
 		
 		for (int i = 7; i < header.length; i++) {
 			contribOrder.add(header[i].trim());
-			contribsToInitials.put(header[i].trim(), new ArrayList<String>());
+			contribsToNames.put(header[i].trim(), new ArrayList<String>());
 		}
 		
 		String temp = null;
@@ -48,7 +66,7 @@ public class AuthorCorral {
 				System.err.println("Error - " + err);
 				errors.add(err);
 			} else {
-				String fullname = line[0].trim() + " " + (!"".equals(line[1].trim()) ? (line[1].trim() + " ") : "") + line[2].trim();
+				String fullname = line[0].trim() + " " + (!"".equals(line[1].trim()) ? (line[1].trim().charAt(0) + ". ") : "") + line[2].trim();
 				
 				int ord = authorNamesOrder.indexOf(fullname);
 				if (ord == -1) {
@@ -94,7 +112,9 @@ public class AuthorCorral {
 				boolean foundAtLeastOne = false;
 				for (int i = 7; i < line.length; i++) {
 					if (!"".equals(line[i].trim())) {
-						contribsToInitials.get(header[i]).add(getInitials(line[0].trim(), line[1].trim(), line[2].trim()));
+						if (contribsToNames.get(header[i]).indexOf(fullname) == -1) {
+							contribsToNames.get(header[i]).add(fullname);//getInitials(line[0].trim(), line[1].trim(), line[2].trim()));
+						}
 						foundAtLeastOne = true;
 					}
 				}
@@ -119,25 +139,28 @@ public class AuthorCorral {
 					authorString.append("and ");
 				}
 				authorString.append(authorNamesOrder.get(i));
-				for (String[] dept : myDepts) {
+				for (int d = 0; d < myDepts.size(); d++) {
+					if (d == 0 && rtfOutput) {
+						authorString.append("#");
+					}
 					StringBuilder deptStr = new StringBuilder();
 					int nonEmptyCnt = 0;
-					if (!"".equals(dept[0])) {
-						deptStr.append(dept[0]);
+					if (!"".equals(myDepts.get(d)[0])) {
+						deptStr.append(myDepts.get(d)[0]);
 						nonEmptyCnt++;
 					}
-					if (!"".equals(dept[1])) {
-						if (nonEmptyCnt > 0 && !"".equals(dept[1])) {
+					if (!"".equals(myDepts.get(d)[1])) {
+						if (nonEmptyCnt > 0 && !"".equals(myDepts.get(d)[1])) {
 							deptStr.append(", ");
 						}
-						deptStr.append(dept[1]);
+						deptStr.append(myDepts.get(d)[1]);
 						nonEmptyCnt++;
 					}
-					if (!"".equals(dept[2])) {
-						if (nonEmptyCnt > 0 && !"".equals(dept[2])) {
+					if (!"".equals(myDepts.get(d)[2])) {
+						if (nonEmptyCnt > 0 && !"".equals(myDepts.get(d)[2])) {
 							deptStr.append(", ");
 						}
-						deptStr.append(dept[2]);
+						deptStr.append(myDepts.get(d)[2]);
 						nonEmptyCnt++;
 					}
 					int index = deptOrder.indexOf(deptStr.toString());
@@ -145,7 +168,12 @@ public class AuthorCorral {
 						index = deptOrder.size();
 						deptOrder.add(deptStr.toString()); 
 					}
-					authorString.append("'").append(index + 1);
+					authorString.append(index + 1);
+					if (d == myDepts.size() - 1 && rtfOutput) {
+						authorString.append("_");
+					} else if (d >= 0 && d < myDepts.size() - 1) {
+						authorString.append(",");
+					}
 				}
 				if (i < authorNamesOrder.size() - 1) {
 					authorString.append(", ");
@@ -157,23 +185,32 @@ public class AuthorCorral {
 			}
 		}
 		
-		outWriter.println(authorString.toString());
-		outWriter.println();
-		for (int i = 0; i < deptOrder.size(); i++) {
-			outWriter.println((i + 1) + " " + deptOrder.get(i));
+		if (rtfOutput) {
+			outWriter.println("{\\rtf1 ");
 		}
-		outWriter.println();
-		outWriter.println();
+		outWriter.println(rtfOutput ? rtfFormatted(authorString.toString(), true) : authorString.toString());
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		for (int i = 0; i < deptOrder.size(); i++) {
+			outWriter.println(rtfOutput ? rtfFormatted((i + 1) + " " + deptOrder.get(i), false) : (i + 1) + " " + deptOrder.get(i));
+			if (rtfOutput) {
+				outWriter.println(rtfOutput ? PARAGRAH : "");
+			}
+		}
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		outWriter.println(rtfOutput ? PARAGRAH : "");
 		
-		outWriter.println("Author Contributions");
+		outWriter.println(rtfOutput ? rtfFormatted("Author Contributions", false) : "Author Contributions");
+		outWriter.print(rtfOutput ? PARAGRAH : "");
 		StringBuilder contribString = new StringBuilder();
 		for (String contrib : contribOrder) {
-			ArrayList<String> contribInitials = contribsToInitials.get(contrib);
+			ArrayList<String> contribInitials = contribsToNames.get(contrib);
 			for (int i = 0; i < contribInitials.size(); i++) {
 				if (i == contribInitials.size() - 1) {
 					contribString.append("and ");
 				}
-				contribString.append(contribInitials.get(i));
+				String[] parts = fullnameToParts.get(contribInitials.get(i));
+				contribString.append(getInitials(parts[0], parts[1], parts[2]));
 				if (i < contribInitials.size() - 1) {
 					contribString.append(", ");
 				} else {
@@ -183,12 +220,13 @@ public class AuthorCorral {
 			contribString.append(contrib).append(". ");
 		}
 		contribString.append("All authors were given the opportunity to comment and provide revisions to the manuscript text.");
-		outWriter.println(contribString.toString());
+		outWriter.println(rtfOutput ? rtfFormatted(contribString.toString(), false) : contribString.toString());
 
-		outWriter.println();
-		outWriter.println();
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		outWriter.println(rtfOutput ? PARAGRAH : "");
 		
-		outWriter.println("Email list");
+		outWriter.println(rtfOutput ? rtfFormatted("Email list", false) : "Email list");
+		outWriter.print(rtfOutput ? PARAGRAH : "");
 		StringBuilder emailString = new StringBuilder();
 		HashSet<String> noEmailSet = new HashSet<String>();
 		int outerCount = 0;
@@ -225,11 +263,12 @@ public class AuthorCorral {
 				noEmailSet.add(noEmailString.toString());
 			}
 		}
-		outWriter.println(emailString.toString());
-		outWriter.println();
+		outWriter.println(rtfOutput ? rtfFormatted(emailString.toString(), false) : emailString.toString());
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		outWriter.println(rtfOutput ? PARAGRAH : "");
 		
-		
-		outWriter.println("No Email Found");
+		outWriter.println(rtfOutput ? rtfFormatted("No Email Found", false) : "No Email Found");
+		outWriter.print(rtfOutput ? PARAGRAH : "");
 		StringBuilder noEmailString = new StringBuilder();
 		int cnt = 0;
 		for (String name : noEmailSet) {
@@ -239,16 +278,21 @@ public class AuthorCorral {
 			noEmailString.append(name);
 			cnt++;
 		}
-		outWriter.println(noEmailString.toString());
-		
-		outWriter.println();
-		outWriter.println();
+		outWriter.println(rtfOutput ? rtfFormatted(noEmailString.toString(), false) : noEmailString.toString());
+
+		outWriter.println(rtfOutput ? PARAGRAH : "");
+		outWriter.println(rtfOutput ? PARAGRAH : "");
 		
 		if (errors.size() > 0) {
-			outWriter.println("Transcription Errors");
+			outWriter.println(rtfOutput ? rtfFormatted("Transcription Errors", false) : "Transcription Errors");
+			outWriter.print(rtfOutput ? PARAGRAH : "");
 		}
 		for (String error : errors) {
-			outWriter.println(error);
+			outWriter.println(rtfOutput ? rtfFormatted(error, false) : error);
+			outWriter.print(rtfOutput ? PARAGRAH : "");
+		}
+		if (rtfOutput) {
+			outWriter.println("}");
 		}
 		
 		outWriter.flush();
@@ -257,26 +301,30 @@ public class AuthorCorral {
 	
 	private static String getInitials(String first, String midTemp, String last) {
 		StringBuilder init = new StringBuilder();
-		for (String s : first.split("[\\s]+")) {
-			init.append(s.charAt(0));
-		}
-		boolean allUpper = true;
-		String mid = midTemp.replaceAll("\\.", "");
-		for (int i = 0; i < mid.length(); i++) {
-			if (!Character.isUpperCase(mid.charAt(i))) {
-				allUpper = false;
-				break;
-			}
-		}
-		if (allUpper) {
-			init.append(mid);
-		} else {
-			for (String s : mid.split("[\\s]+")) {
+		try {
+			for (String s : first.split("[\\s]+")) {
 				init.append(s.charAt(0));
 			}
-		}
-		for (String s : last.split("[\\s]+")) {
-			init.append(s.charAt(0));
+			boolean allUpper = true;
+			String mid = midTemp.replaceAll("\\.", "");
+			for (int i = 0; i < mid.length(); i++) {
+				if (!Character.isUpperCase(mid.charAt(i))) {
+					allUpper = false;
+					break;
+				}
+			}
+			if (allUpper) {
+				init.append(mid);
+			} else {
+				for (String s : mid.split("[\\s]+")) {
+					init.append(s.charAt(0));
+				}
+			}
+			for (String s : last.split("[\\s]+")) {
+				init.append(s.charAt(0));
+			}
+		} catch (Exception e) {
+			System.err.println("Error - problem getting initials from [" + first + "; " + midTemp + "; " + last + "]");
 		}
 		return init.toString().trim();
 	}
@@ -284,12 +332,17 @@ public class AuthorCorral {
 	public static void main(String[] args) {
 		int numArgs = args.length;
 		String inFile = "N:/statgen/authors/input2.xln";
-		String outFile = "N:/statgen/authors/authorship.out";
-
+		String outFile = "N:/statgen/authors/authorship.rtf";
+		boolean rtf = false;
+		boolean err = true;
+		
 		String usage =  "\n" + 
-						"one.AuthorCorral requires 2 arguments\n" + 
+						"one.AuthorCorral requires 2+ arguments\n" + 
 						"   (1) input filename (i.e. file=" + inFile + " (default))\n" + "" + 
-						"   (1) output filename (i.e. out=" + outFile + " (default))\n" + "";
+						"   (2) output filename (i.e. out=" + outFile + " (default))\n" + "" + 
+						"   (3) OPTIONAL output in RTF format (used for superscripts) (i.e. rtf=" + rtf + " (default))\n" + 
+						"   (4) OPTIONAL include errors in output (i.e. err=" + err + " (default))\n" + "";
+		
 
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("-h") || args[i].equals("-help") || args[i].equals("/h") || args[i].equals("/help")) {
@@ -301,6 +354,12 @@ public class AuthorCorral {
 			} else if (args[i].startsWith("out=")) {
 				outFile = args[i].split("=")[1];
 				numArgs--;
+			} else if (args[i].startsWith("rtf=")) {
+				rtf = Boolean.valueOf(args[i].split("=")[1]);
+				numArgs--;
+			} else if (args[i].startsWith("err=")) {
+				err = Boolean.valueOf(args[i].split("=")[1]);
+				numArgs--;
 			} else {
 				System.err.println("Error - invalid argument: " + args[i]);
 			}
@@ -310,7 +369,7 @@ public class AuthorCorral {
 			System.exit(1);
 		}
 		try {
-			run(inFile, outFile);
+			run(inFile, outFile, rtf, err);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
