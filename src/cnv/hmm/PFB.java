3acf3b1c04206bd3644033832adbf541dd587d8d
jlanej <lane0212@umn.edu>
2015-09-22 14:47:29 -0500

    Added CDF to GaussianDistributions
    PennHmm: towards a replicate of PennCNV

---
+++ src/cnv/hmm/PFB.java
@@ -0,0 +1,57 @@
+package cnv.hmm;
+
+import java.io.FileNotFoundException;
+
+import cnv.filesys.Project;
+import cnv.manage.ExtProjectDataParser;
+import cnv.manage.ExtProjectDataParser.Builder;
+
+/**
+ * @author lane0212 Handles the pfb data for the hmm in reduced format
+ */
+public class PFB {
+	private Project proj;
+	private double[] pfbs;
+
+	private PFB(Project proj, double[] pfbs) {
+		super();
+		this.proj = proj;
+		this.pfbs = pfbs;
+		if (pfbs.length != proj.getMarkerNames().length) {
+			String error = "Found " + pfbs.length + " pfb entries, but the project has" + pfbs.length + " markers";
+			proj.getLog().reportTimeError(error);
+			throw new IllegalArgumentException(error);
+		}
+	}
+	
+	
+
+	public static PFB loadPFB(Project proj) {
+		return loadPFB(proj, proj.CUSTOM_PFB_FILENAME.getValue());
+	}
+	
+	public static PFB loadPFB(Project proj, String fullPathToPfb) {
+
+		Builder builder = new Builder();
+		builder.dataKeyColumnName("Name");
+		builder.numericDataTitles(new String[] { "PFB" });
+		builder.sampleBased(false);
+		builder.requireAll(true);
+		builder.treatAllNumeric(false);
+
+		try {
+			ExtProjectDataParser extProjectDataParser = builder.build(proj, fullPathToPfb);
+			extProjectDataParser.determineIndicesFromTitles();
+			extProjectDataParser.loadData();
+			double[] pfbs = extProjectDataParser.getNumericDataForTitle("PFB");
+			return new PFB(proj, pfbs);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			proj.getLog().reportFileNotFound(fullPathToPfb);
+			return null;
+		}
+
+	}
+
+}
