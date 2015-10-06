5b0aa36fc0582ea2356e08f9d1e074a3466bd525
jlanej <lane0212@umn.edu>
2015-10-06 14:22:17 -0500

    Sorry, will be an error with da Gauss

--- jahmm/src/be/ac/ulg/montefiore/run/distributions/GaussianDistribution.java
+++ jahmm/src/be/ac/ulg/montefiore/run/distributions/GaussianDistribution.java
@@ -24,6 +24,10 @@ package be.ac.ulg.montefiore.run.distributions;
 
 import java.util.*;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+import cnv.var.CNVariant;
+
 
 /**
  * This class implements a Gaussian distribution.
@@ -92,19 +96,19 @@ public class GaussianDistribution implements RandomDistribution {
 	 * @return the cdf of this value using taylor aproximation, custom genvisis code.
 	 */
 	public double cdf(double n) {
-		// return new NormalDistribution(mean, deviation).cumulativeProbability(n);
-
-		double z = (n - mean) / deviation;
-		if (z < -8.0)
-			return 0.0;
-		if (z > 8.0)
-			return 1.0;
-		double sum = 0.0, term = z;
-		for (int i = 3; sum + term != sum; i += 2) {
-			sum = sum + term;
-			term = term * z * z / i;
-		}
-		return 0.5 + sum * probability(z);
+		return new NormalDistribution(mean, deviation).cumulativeProbability(n);
+		//
+//		double z = (n - mean) / deviation;
+//		if (z < -8.0)
+//			return 0.0;
+//		if (z > 8.0)
+//			return 1.0;
+//		double sum = 0.0, term = z;
+//		for (int i = 3; sum + term != sum; i += 2) {
+//			sum = sum + term;
+//			term = term * z * z / i;
+//		}
+//		return 0.5 + sum * probability(z);
 	}
 
 	private static final long serialVersionUID = 9127329839769283975L;
