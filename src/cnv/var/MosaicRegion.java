2a345bf4e0fefb03eb4bd44eaf5e7f04e7865ef1
jlanej <lane0212@umn.edu>
2015-10-27 11:55:10 -0500

    Mosaicism: uses bp weighted metric

--- src/cnv/var/MosaicRegion.java
+++ src/cnv/var/MosaicRegion.java
@@ -8,14 +8,20 @@ public class MosaicRegion extends CNVariant {
 	private static final long serialVersionUID = 1L;
 
 	private double nearestStateScore;
+	private double bpWeightedScore;
 
-	public MosaicRegion(CNVariant cnv, double nearestStateScore) {
+	public MosaicRegion(CNVariant cnv, double bpWeightedScore, double nearestStateScore) {
 		super(cnv);
+		this.bpWeightedScore = bpWeightedScore;
 		this.nearestStateScore = nearestStateScore;
 	}
 
 	public double getNearestStateScore() {
 		return nearestStateScore;
+	}
+
+	public double getBpWeightedScore() {
+		return bpWeightedScore;
 	}
 
 }
