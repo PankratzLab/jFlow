1c689bc50009ab334badd9cb829fe5b3fb5d7c41
jlanej <lane0212@umn.edu>
2015-11-17 12:40:30 -0600

    Now store the evalue Histogram in blast results

---
+++ src/cnv/annotation/MarkerEvalueHistogramAnnotation.java
@@ -0,0 +1,129 @@
+package cnv.annotation;
+
+import htsjdk.variant.variantcontext.VariantContext;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import common.Array;
+import common.Logger;
+import common.Sort;
+import stats.Histogram.DynamicHistogram;
+
+/**
+ * TODO, gotta be a better way to track without the string conversions
+ *
+ *
+ */
+public class MarkerEvalueHistogramAnnotation extends HistogramAnnotation {
+
+	public static final String DEFAULT_NAME = "BLAST_EVALUE_COUNTS";
+	public static final String DEFUALT_DELIM = "/";
+	public static final String DEFAULT_DESCRIPTION = "A list of evalues and counts for all blast hits a marker";
+	private List<String> blastEvalueCounts;
+	private List<String> blastEvalues;
+	private Hashtable<String, Integer> exactHistogram;
+
+	public MarkerEvalueHistogramAnnotation(String name, String description) {
+		super(name, description, new DynamicHistogram(0, 1, 2));
+		this.exactHistogram = new Hashtable<String, Integer>();
+	}
+
+	public void setDataToExactHistogram() {
+		double[] tmp = new double[exactHistogram.keySet().size()];
+		int index = 0;
+
+		for (String key : exactHistogram.keySet()) {
+			tmp[index] = Double.parseDouble(key);
+			index++;
+		}
+		int[] indices = Sort.trickSort(tmp);
+		ArrayList<String> data = new ArrayList<String>();
+		for (int i = 0; i < indices.length; i++) {
+			data.add(tmp[indices[i]] + "");
+			data.add(exactHistogram.get(tmp[indices[i]] + "") + "");
+		}
+		setData(Array.toStr(Array.toStringArray(data), DEFUALT_DELIMITER));
+	}
+
+	public void addExactHistogram(double d) {
+		String tmp = d + "";
+		if (exactHistogram.containsKey(tmp)) {
+			exactHistogram.put(tmp, exactHistogram.get(tmp) + 1);
+		} else {
+			exactHistogram.put(tmp, 1);
+		}
+	}
+
+	public static Annotation getDefaultBlastAnnotation() {
+		return new HistogramAnnotation(DEFAULT_NAME, DEFAULT_DESCRIPTION, null) {
+		};
+	}
+
+	@Override
+	public void parseAnnotation(VariantContext vc, Logger log) {
+		if (vc.hasAttribute(getName())) {
+			setData(vc.getAttributeAsString(getName(), DEFAULT_NAME));
+			List<String> tmp = getDataAsList();
+			ArrayList<String> tmpCounts = new ArrayList<String>();
+			ArrayList<String> tmpEvals = new ArrayList<String>();
+			for (int i = 0; i < tmp.size(); i += 2) {
+				tmpCounts.add(tmp.get(i));
+				tmpEvals.add(tmp.get(i + 1));
+			}
+			blastEvalueCounts = tmpCounts;
+			blastEvalues = tmpEvals;
+			setFound(true);
+		}
+	}
+
+	/**
+	 * Not really a histogram since the values are not binned, but thats cuz I didn't know the best way to bin
+	 *
+	 */
+	public static class EvalueHistogram {
+		private double[] evalues;
+		private int[] counts;
+
+		public EvalueHistogram(double[] evalues, int[] counts) {
+			super();
+			this.evalues = evalues;
+			this.counts = counts;
+		}
+
+		public int[] getCounts() {
+			return counts;
+		}
+
+		public double[] getEvalues() {
+			return evalues;
+		}
+
+	}
+
+	public EvalueHistogram formatHistogram() {
+		if (blastEvalueCounts == null || blastEvalues == null || blastEvalueCounts.size() != blastEvalues.size()) {
+			throw new IllegalStateException("Invalid sizes for counts and evalue arrays");
+
+		} else {
+			double[] evalues = new double[blastEvalues.size()];
+			int[] counts = new int[blastEvalueCounts.size()];
+			for (int i = 0; i < counts.length; i++) {
+				counts[i] = Integer.parseInt(blastEvalueCounts.get(i));
+				evalues[i] = Double.parseDouble(blastEvalues.get(i));
+			}
+			return new EvalueHistogram(evalues, counts);
+		}
+
+	}
+
+	public List<String> getBlastEvalues() {
+		return blastEvalues;
+	}
+
+	public List<String> getBlastEvalueCounts() {
+		return blastEvalueCounts;
+	}
+
+}
