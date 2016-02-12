c5ed265d0064140cbcc6f0b1e0a9d2e5721a8dad
cole0482 <cole0482@umn.edu>
2016-02-12 17:38:23 -0600

    EmimPipeline: add class to git; Emim: setup for use with EmimPipeline; ExportCNVsToPedFormat: use all info from pedigree.dat file;

--- src/gwas/Emim.java
+++ src/gwas/Emim.java
@@ -109,47 +109,112 @@ public class Emim {
 	    reader.close();
 	}
 
-	private static void scriptAll(String plinkPrefix, String excludeFile) {
-		String commands;
-		String currDir;
-		
-		if (excludeFile.equals("GEN")) {
-		    try {
-                listSexMarkers(plinkPrefix + ".bim", "sexChrMarkers.txt");
+	protected static void scriptAllInDir(String runDir, String plinkDirAndRoot, String relativePlinkRoot, String excludeFile, String keepFile, double pThreshold) {
+	    String commands;
+        String currDir = ext.verifyDirFormat(runDir);
+        
+        if (currDir.charAt(0) != '/' && !currDir.contains(":")) {
+            currDir = (new File("./" + currDir)).getAbsolutePath() + "/";
+        }
+        
+        if (excludeFile.equals("GEN")) {
+            try {
+                listSexMarkers(plinkDirAndRoot + ".bim", currDir + "sexChrMarkers.txt");
                 excludeFile = "sexChrMarkers.txt";
             } catch (NumberFormatException | IOException e) {
                 excludeFile = null;
                 e.printStackTrace();
             }
-		}
-		
-		currDir = (new File("./")).getAbsolutePath();
-		commands = "cd " + currDir + "\n" + 
-		        "plink2 --noweb --bfile " + plinkPrefix + (excludeFile != null ? " --exclude " + excludeFile : "") + " --make-bed --out emimPrep\n"+
-				"plink2 --noweb --bfile emimPrep --mendel\n"+
-				"plink2 --noweb --bfile emimPrep --tdt\n"+
-				"premim -cg -a -rout risksnplist.txt emimPrep.bed\n"+
-				"\n"+
-				"jcp gwas.Emim run=C\n"+
-				"emim\n"+
-				"mv emimsummary.out emimsummary_C.out\n"+
-				"mv emimresults.out emimresults_C.out\n"+
-				"cp emimparams.dat emimparams_C.dat\n"+
-				"\n"+
-				"jcp gwas.Emim run=CM\n"+
-				"emim\n"+
-				"mv emimsummary.out emimsummary_CM.out\n"+
-				"mv emimresults.out emimresults_CM.out\n"+
-				"cp emimparams.dat emimparams_CM.dat\n"+
-				"\n"+
-				"jcp gwas.Emim run=M\n"+
-				"emim\n"+
-				"mv emimsummary.out emimsummary_M.out\n"+
-				"mv emimresults.out emimresults_M.out\n"+
-				"cp emimparams.dat emimparams_M.dat\n"+
-				"# run jcp gwas.ResultsPackager"+"\n";
-		
-		Files.qsub(plinkPrefix+"_runEmim.pbs", commands, 5000, 24, 1);
+        }
+        
+        commands = "cd " + currDir + "\n" + 
+                "plink2 --noweb --bfile " 
+                        + relativePlinkRoot
+                        + (excludeFile != null ? " --exclude " + excludeFile : "")  
+                        + (keepFile != null ? " --keep " + keepFile : "")  
+                        + " --make-bed --out emimPrep\n"+
+                "mv plink.log plink_prep.log\n"+
+                "\n"+
+                "plink2 --noweb --bfile emimPrep --mendel\n"+
+                "mv plink.log plink_mendel.log\n"+
+                "\n"+
+                "plink2 --noweb --bfile emimPrep --tdt\n"+
+                "mv plink.log plink_tdt.log\n"+
+                "\n"+
+                "plink2 --noweb --bfile emimPrep --hardy\n"+
+                "premim -cg -a -rout risksnplist.txt emimPrep.bed\n"+
+                "\n"+
+                "jcp gwas.Emim run=C\n"+
+                "emim\n"+
+                "mv emimsummary.out emimsummary_C.out\n"+
+                "mv emimresults.out emimresults_C.out\n"+
+                "cp emimparams.dat emimparams_C.dat\n"+
+                "\n"+
+                "jcp gwas.Emim run=CM\n"+
+                "emim\n"+
+                "mv emimsummary.out emimsummary_CM.out\n"+
+                "mv emimresults.out emimresults_CM.out\n"+
+                "cp emimparams.dat emimparams_CM.dat\n"+
+                "\n"+
+                "jcp gwas.Emim run=M\n"+
+                "emim\n"+
+                "mv emimsummary.out emimsummary_M.out\n"+
+                "mv emimresults.out emimresults_M.out\n"+
+                "cp emimparams.dat emimparams_M.dat\n"+
+                "\n"+
+                "jcp gwas.Emim parse=./ hwe=plink.hwe pThreshold=" + pThreshold;
+        
+        Files.qsub(currDir + ext.rootOf(plinkDirAndRoot, true)+"_runEmim.pbs", commands, 5000, 24, 1);
+	}
+	
+	public static void scriptAll(String plinkPrefix, String excludeFile, String keepFile, double pThreshold) {
+	    scriptAllInDir("./", plinkPrefix, plinkPrefix, excludeFile, keepFile, pThreshold);
+//		String commands;
+//		String currDir;
+//		
+//		if (excludeFile.equals("GEN")) {
+//		    try {
+//                listSexMarkers(plinkPrefix + ".bim", "sexChrMarkers.txt");
+//                excludeFile = "sexChrMarkers.txt";
+//            } catch (NumberFormatException | IOException e) {
+//                excludeFile = null;
+//                e.printStackTrace();
+//            }
+//		}
+//		
+//		currDir = (new File("./")).getAbsolutePath();
+//		commands = "cd " + currDir + "\n" + 
+//		        "plink2 --noweb --bfile " 
+//		                + plinkPrefix 
+//		                + (excludeFile != null ? " --exclude " + excludeFile : "")  
+//		                + (keepFile != null ? " --keep " + keepFile : "")  
+//		                + " --make-bed --out emimPrep\n"+
+//				"plink2 --noweb --bfile emimPrep --mendel\n"+
+//				"plink2 --noweb --bfile emimPrep --tdt\n"+
+//				"plink2 --noweb --bfile emimPrep --hardy\n"+
+//				"premim -cg -a -rout risksnplist.txt emimPrep.bed\n"+
+//				"\n"+
+//				"jcp gwas.Emim run=C\n"+
+//				"emim\n"+
+//				"mv emimsummary.out emimsummary_C.out\n"+
+//				"mv emimresults.out emimresults_C.out\n"+
+//				"cp emimparams.dat emimparams_C.dat\n"+
+//				"\n"+
+//				"jcp gwas.Emim run=CM\n"+
+//				"emim\n"+
+//				"mv emimsummary.out emimsummary_CM.out\n"+
+//				"mv emimresults.out emimresults_CM.out\n"+
+//				"cp emimparams.dat emimparams_CM.dat\n"+
+//				"\n"+
+//				"jcp gwas.Emim run=M\n"+
+//				"emim\n"+
+//				"mv emimsummary.out emimsummary_M.out\n"+
+//				"mv emimresults.out emimresults_M.out\n"+
+//				"cp emimparams.dat emimparams_M.dat\n"+
+//				"\n"+
+//				"jcp gwas.Emim parse=./ hwe=plink.hwe pThreshold=" + pThreshold;
+//		
+//		Files.qsub(plinkPrefix+"_runEmim.pbs", commands, 5000, 24, 1);
 	}
 	
 	public static void parse(String dir, String hweFile, double pValueThreshold) {
@@ -196,6 +261,7 @@ public class Emim {
 		String plinkPrefix = null;
 		boolean allelic = true;
 		String excludeFile = "GEN";
+		String keepFile = null;
 
 		String usage = "\n" +
 		"gwas.Emim requires 0-1 arguments\n" +
@@ -203,6 +269,9 @@ public class Emim {
 		"   (2) allelic instead of 2df genotypic test (i.e. allelic=" + allelic + " (default))\n" +
 		"  OR\n" +
 		"   (1) generate script that runs the full process (i.e. script=plinkPrefix (not the default))\n" + 
+		"   (2) p-value threshold to filter on (piped to parse method) (i.e. pThreshold=" + pValueThreshold + " (default))\n" + 
+		"   (2) (optional) a file of markers to exclude - the default will generate a file of sex markers, as Emim won't parse these. (i.e. exclude=drops.dat (not the default))\n" + 
+		"   (3) (optional) a file of tab-delimited FID/IID pairs to keep (i.e. keep=completeTrios.dat (not the default))\n" + 
 		"  OR\n" +
 		"   (1) directory to parse (i.e. parse=./ (not the default))\n" + 
 		"   (2) p-value threshold to filter on (i.e. pThreshold=" + pValueThreshold + " (default))\n" + 
@@ -221,6 +290,9 @@ public class Emim {
 				numArgs--;
 			} else if (args[i].startsWith("exclude=")) {
 			    excludeFile = ext.parseStringArg(args[i], null);
+			    numArgs--;
+			} else if (args[i].startsWith("keep=")) {
+			    keepFile = ext.parseStringArg(args[i], null);
 			    numArgs--;
 			} else if (args[i].startsWith("parse=")) {
 				dir = ext.parseStringArg(args[i], null);
@@ -246,7 +318,7 @@ public class Emim {
 			if (dir != null) {
 				parse(dir, hweFile, pValueThreshold);
 			} else if (plinkPrefix != null) {
-				scriptAll(plinkPrefix, excludeFile);
+				scriptAll(plinkPrefix, excludeFile, keepFile, pValueThreshold);
 			} else {
 				setTo(runType, allelic);
 			}
