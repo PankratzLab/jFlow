247a0b116962fc2d045babedc25495fa1bd98d83
jlanej <lane0212@umn.edu>
2016-03-29 13:06:38 -0500

    Few uncommitted things that have been sitting around

---
+++ src/seq/analysis/TumorNormalLOH.java
@@ -0,0 +1,17 @@
+package seq.analysis;
+
+
+
+/**
+ * Scan genotypes from GATK HC to look for any evidence of LOH
+ *
+ */
+public class TumorNormalLOH {
+
+//	private static void scan(String vcf, String vpopFile, String outputDir) {
+//		new File(outputDir).mkdirs();
+//		Logger log = new Logger(outputDir + "lohscan.log");
+//
+//	}
+
+}
