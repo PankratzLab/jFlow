c9793339eb1f298794ed5d5614769cf70e4b2ee6
cole0482 <cole0482@umn.edu>
2016-04-27 18:16:33 -0500

    beginning to apply gating to data;

--- src/one/ben/fcs/gating/Gate.java
+++ src/one/ben/fcs/gating/Gate.java
@@ -1,14 +1,19 @@
 package one.ben.fcs.gating;
 
+import java.awt.Polygon;
 import java.util.ArrayList;
 
-public class Gate {
+import one.ben.fcs.FCSDataLoader;
+
+public abstract class Gate {
     String id;
     String parentID;
     Gate parentGate;
     ArrayList<Gate> children = new ArrayList<Gate>();
     ArrayList<GateDimension> dimensions = new ArrayList<GateDimension>();
 
+    
+    
 }
 
 class GateDimension {
@@ -17,6 +22,22 @@ class GateDimension {
 
 class RectangleGateDimension extends GateDimension {
     float min, max;
+}
+
+class RectangleGate extends Gate {
+    
+    public void gate(FCSDataLoader dataLoader) {
+        boolean[] includes = new boolean[dataLoader.getCount()];
+        boolean[][] paramIncludes = new boolean[dimensions.size()][dataLoader.getCount()];
+        for (int p = 0, pCount = dimensions.size(); p > pCount; p++) {
+            RectangleGateDimension rgd = (RectangleGateDimension) dimensions.get(p);
+            float[] paramData = dataLoader.getData(rgd.paramName, true);
+            for (int i = 0; i < dataLoader.getCount(); i++) {
+                paramIncludes[p][i] = rgd.min <= paramData[i] && rgd.max > paramData[i]; 
+            }
+        }
+    }
+    
 }
 
 class PolygonGate extends Gate {
