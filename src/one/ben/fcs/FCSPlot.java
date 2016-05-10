62a12707bad2fad5592b12195df6271023359c30
Ben <rcoleb@gmail.com>
2016-05-10 15:24:19 -0500

    Biexponential axes (not quite complete)

--- src/one/ben/fcs/FCSPlot.java
+++ src/one/ben/fcs/FCSPlot.java
@@ -452,11 +452,11 @@ public class FCSPlot extends JPanel implements WindowListener, ActionListener, P
         frame.pack();
         frame.setVisible(show);
 
-//        String fcsFilename = "F:\\Flow\\P1 PBMCs A&C ZF 31616_ULTRA BRIGHT RAINBOW BEADS_URB_002.fcs";
+        String fcsFilename = "C:\\workspace\\2016-05-04_URB_DHS_ULTRA BRIGHT RAINBOW BEADS_URB_001.fcs";
 //        String fcsFilename = "F:\\Flow\\P1-B&C-CD3-APC-Cy7 or CD4-APC-Cy7_ULTRA BRIGHT RAINBOW BEADS_URB_001.fcs";
 //        String fcsFilename = "F:\\Flow\\P1- PBMC-A&C rest_panel one_PBMC-C P1 1HR rest_003.fcs";
 //        String fcsFilename = "F:\\Flow\\P1- PBMC-A&C rest_panel one_PBMC-A P1 1HR rest_002.fcs";
-//        twoDPlot.loadFile(fcsFilename);
+        twoDPlot.loadFile(fcsFilename);
         
 		return twoDPlot;
     }
