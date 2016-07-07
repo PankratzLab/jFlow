f825902c4985a8401faa58f9455a965b176053aa
jlanej <lane0212@umn.edu>
2016-07-07 11:20:02 -0500

    Placeholder for SRA runs

---
+++ src/seq/manage/SRAManager.java
@@ -0,0 +1,16 @@
+package seq.manage;
+
+/**
+ * @author lane0212
+ *
+ *         Going to be the manager for the sra download, process, and delete
+ *
+ *         Things to run:<br>
+ *         TelSeq<br>
+ *         Computel<br>
+ *         mtDNA CN<br>
+ *         Genvisis import<br>
+ */
+public class SRAManager {
+
+}
