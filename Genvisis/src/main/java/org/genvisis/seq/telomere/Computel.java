83ffc584c825754fdbedd2d9c1f5cc4e249f228c
jlanej <jlanej@gmail.com>
2016-07-30 17:54:11 -0500

    Hello Internet

---
+++ Genvisis/src/main/java/org/genvisis/seq/telomere/Computel.java
@@ -0,0 +1,11 @@
+package org.genvisis.seq.telomere;
+
+
+
+/**
+ *Wrapper for the computel telomere length estimator ala https://github.com/lilit-nersisyan/computel
+ *
+ */
+public class Computel {
+
+}
