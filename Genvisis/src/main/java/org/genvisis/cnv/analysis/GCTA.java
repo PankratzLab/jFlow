156ed092e48433d2825bf2d9f208b3f1c827aee4
jlanej <jlanej@gmail.com>
2016-08-01 11:34:39 -0500

    GCTA init

---
+++ Genvisis/src/main/java/org/genvisis/cnv/analysis/GCTA.java
@@ -0,0 +1,15 @@
+package org.genvisis.cnv.analysis;
+
+
+/**
+ * Class for running GCTA (http://cnsgenomics.com/software/gcta/) focusing on heritability
+ *
+ */
+public class GCTA {
+	
+	
+	
+	
+	
+
+}
