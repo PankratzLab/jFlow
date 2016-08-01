fff3e3232f6cc482a9e4d01614ee739e5167f17c
jlanej <jlanej@gmail.com>
2016-08-01 12:00:32 -0500

    GCTA docs were worth the read

--- Genvisis/src/main/java/org/genvisis/cnv/analysis/GCTA.java
+++ Genvisis/src/main/java/org/genvisis/cnv/analysis/GCTA.java
@@ -2,14 +2,52 @@ package org.genvisis.cnv.analysis;
 
 
 /**
- * Class for running GCTA (http://cnsgenomics.com/software/gcta/) focusing on heritability
+ * Class for running GCTA (http://cnsgenomics.com/software/gcta/) focusing on
+ * heritability
  *
  */
 public class GCTA {
-	
-	
-	
-	
-	
+
+	// private Project proj;
+	// /**
+	// * Samples to use, should already have been qc'ed
+	// */
+	// private String[] samples;
+	// /**
+	// * Markers to use, also should already have been qc'ed
+	// */
+	// private String[] markers;
+	//
+
+//	private String plinkRoot;
+//
+//	/**
+//	 * Maf filter for any markers used
+//	 */
+//	private double maf;
+//	/**
+//	 * Even remotely related pairs of individuals (genetic similarity greater
+//	 * than 0.025, which represents fifth-degree relatives) are excluded so that
+//	 * chance genetic similarity is used as a random effect in a linear mixed
+//	 * model.
+//	 */
+//	private double crypticRelatedParam;
+//	/**
+//	 * Full path to plink root (i.e /home/usr/data/plinkFileRoot
+//	 */
+//
+//	/**
+//	 * Set to -1 to run all chrs
+//	 */
+//	private byte chr;
+//	private int numthreads;
+//
+//	private String output;
+
+	private static void runGCTA(String plinkRoot, String output, double crypticRelatedParam, double maf, byte chr,
+			int numthreads) {
+		
+
+	}
 
 }
