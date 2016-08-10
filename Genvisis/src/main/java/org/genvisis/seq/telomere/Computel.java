90f167a38485df209f4b4c6f3b9bdb74c3c2847b
jlanej <jlanej@gmail.com>
2016-08-10 16:06:59 -0500

    ComputelConfig

--- Genvisis/src/main/java/org/genvisis/seq/telomere/Computel.java
+++ Genvisis/src/main/java/org/genvisis/seq/telomere/Computel.java
@@ -1,11 +1,15 @@
 package org.genvisis.seq.telomere;
 
-
-
 /**
- *Wrapper for the computel telomere length estimator ala https://github.com/lilit-nersisyan/computel
+ * Wrapper for the computel telomere length estimator ala
+ * https://github.com/lilit-nersisyan/computel
  *
  */
 public class Computel {
-
+	
+	
+	
+	// bedtools bamtofastq -i aln.qsort.bam \
+	// -fq aln.end1.fq \
+	// -fq2 aln.end2.fq
 }
