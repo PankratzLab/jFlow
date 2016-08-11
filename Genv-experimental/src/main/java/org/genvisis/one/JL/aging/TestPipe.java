395f5abca4fc446e843cd79d020fc10d076f8b29
jlanej <jlanej@gmail.com>
2016-08-11 09:19:06 -0500

    Playground

--- Genv-experimental/src/main/java/org/genvisis/one/JL/aging/TestPipe.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/aging/TestPipe.java
@@ -1,5 +1,7 @@
 package org.genvisis.one.JL.aging;
 
+import java.io.File;
+
 import org.genvisis.common.Logger;
 import org.genvisis.common.ext;
 import org.genvisis.seq.analysis.MitoSeqCN;
@@ -18,10 +20,15 @@ public class TestPipe {
 	public static void main(String[] args) {
 		SRARunTable sraRunTable = SRARunTable.load("/Volumes/Beta/data/aric_sra/prep/SraRunTable.txt", new Logger());
 		String targetBam = "/Volumes/Beta/data/aric_sra/bams/SRR1654226.bam";
+		String outDir = "/Volumes/Beta/data/aric_sra/test/";
+		new File(outDir).mkdirs();
 		
+		Logger log = new Logger(outDir+".log");
 		
 		String sraSamp = ext.rootOf(targetBam);
 		System.out.println(sraRunTable.get(sraSamp).toString());
+		
+		
 		
 	//	MitoSeqCN.run(fileOfBams, outDir, captureBed, referenceGenomeFasta, params, numthreads);
 		
