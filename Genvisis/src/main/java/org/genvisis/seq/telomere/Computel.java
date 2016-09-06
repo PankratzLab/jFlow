a61cdd3fed78572774190400ebb19a1f4bc4b716
jlanej <jlanej@gmail.com>
2016-09-06 15:30:27 -0500

    Computel: parse result file to more useable format

--- Genvisis/src/main/java/org/genvisis/seq/telomere/Computel.java
+++ Genvisis/src/main/java/org/genvisis/seq/telomere/Computel.java
@@ -14,7 +14,9 @@ import org.genvisis.CLI;
 import org.genvisis.common.Array;
 import org.genvisis.common.CmdLine;
 import org.genvisis.common.Files;
+import org.genvisis.common.HashVec;
 import org.genvisis.common.Logger;
+import org.genvisis.common.Matrix;
 import org.genvisis.common.Zip;
 import org.genvisis.common.ext;
 import org.genvisis.seq.manage.BamOps;
@@ -170,6 +172,20 @@ public class Computel {
 
           Files.write("blanked", r1);
           Files.write("blanked", r2);
+          String finalResults = finalOutDirectory + "results/tel.length.xls";
+
+          if (Files.exists(finalResults)) {
+            String parsedFinalResults = finalResults + ".parsed";
+            String[][] file = HashVec.loadFileToStringMatrix(parsedFinalResults, false,
+                                                             new int[] {0, 1}, false);
+            for (int i = 0; i < file.length; i++) {
+              for (int j = 0; j < file[i].length; j++) {
+                file[i][j] = file[i][j].replaceAll("\"", "");
+              }
+            }
+            Files.writeMatrix(Matrix.transpose(file), parsedFinalResults, "\t");
+
+          }
 
         }
       }
