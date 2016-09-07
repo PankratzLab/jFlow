b20f3d55bdad2ffef72c5e978995c33a3cba360f
jlanej <jlanej@gmail.com>
2016-09-07 12:17:48 -0500

    Placeholder for Dinumt

---
+++ Genvisis/src/main/java/org/genvisis/seq/analysis/Dinumt.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.genvisis.seq.analysis;
+
+import org.genvisis.seq.analysis.genage.SRAPipeline;
+
+/**
+ * @author Kitty
+ *
+ *         Wrapper for https://github.com/mills-lab/dinumt
+ * 
+ *         May become part of the {@link SRAPipeline}
+ */
+public class Dinumt {
+
+}
