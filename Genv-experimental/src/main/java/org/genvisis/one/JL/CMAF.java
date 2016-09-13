3fb8ae028490dd21586b576052a58dad6cdbec6b
jlanej <jlanej@gmail.com>
2016-09-13 09:16:06 -0500

    Experimental CMAF computation:
    Likely to be too specific (GenBank mitochondrial) sequences

---
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/CMAF.java
@@ -0,0 +1,62 @@
+package org.genvisis.one.JL;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.genvisis.common.Logger;
+import org.genvisis.seq.analysis.VCFSourceReader;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
+
+public class CMAF {
+
+	private static class TrackCmaf extends HashMap<String, Double> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		private TrackCmaf() {
+
+		}
+
+	}
+
+	private static void compute(String vcf, double[] freqs, List<String> groups, int numSamples, Logger log) {
+		VCFFileReader reader = new VCFSourceReader(vcf, false);
+		for (VariantContext vc : reader) {
+			for (String group : groups) {
+				if (vc.hasAttribute(group)) {
+					for (int i = 0; i < freqs.length; i++) {
+						double freq = freqs[i];
+						try {
+							double af = Double.parseDouble(vc.getAttributeAsString("AC", "NaN"));
+
+							if (af <= freq) {
+								log.reportTimeInfo(af + "");
+							}
+						} catch (NumberFormatException nfe) {
+
+						}
+					}
+				}
+			}
+		}
+		reader.close();
+
+	}
+
+	public static void main(String[] args) {
+		String vcf = "/Volumes/Beta/data/Cushings/mito/processDir/polymorphismsMTVariants.pos_1.posAdjust_-1.hg19_multianno.eff.gatk.sed1000g.posAdjust_1.vcf";
+		ArrayList<String> groups = new ArrayList<String>();
+		groups.add("Uniprot_name");
+		groups.add("OXPHOS_complex");
+		double[] freqs = new double[] { 1, .1, .01 };
+		int numSamples = 32059;
+		compute(vcf, freqs, groups, numSamples, new Logger());
+	}
+
+}
