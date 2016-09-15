c2101f6cd478ed6a1ec5a0bafe940459c2710991
jlanej <jlanej@gmail.com>
2016-09-15 17:18:19 -0500

    VCFOpsMT: class focusing on mtDNA variant trickery and annotation

---
+++ Genvisis/src/main/java/org/genvisis/seq/manage/VCFOpsMT.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.genvisis.seq.manage;
+
+import org.genvisis.common.Logger;
+
+/**
+ * Class for managing mitochondrial VCFs
+ *
+ * http://haplogrep.uibk.ac.at/blog/rcrs-vs-rsrs-vs-hg19/
+ *
+ */
+public class VCFOpsMT {
+
+
+  // TODO
+  public static void convertHg19ToRCRS(String inputVCF, String outputVCF, Logger log) {
+
+  }
+
+  // TODO
+  public static void convertRCRSToHG19(String inputVCF, String outputVCF, Logger log) {
+
+  }
+
+  // TODO
+  public static void parseGenBankVCFs(String inputGenBankVCF, String outputGenBankVCF, Logger log) {
+
+  }
+
+
+
+}
