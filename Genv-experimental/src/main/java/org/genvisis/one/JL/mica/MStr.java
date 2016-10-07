cd3c8056e6182d0afd21f316dc10516268ae90a2
jlanej <jlanej@gmail.com>
2016-10-07 17:05:46 -0500

    MultiVCFs

--- Genv-experimental/src/main/java/org/genvisis/one/JL/mica/MStr.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/mica/MStr.java
@@ -21,95 +21,101 @@ import htsjdk.variant.vcf.VCFFileReader;
 
 public class MStr {
 
-	private static void run(String vcf, String outDir) {
-		new File(outDir).mkdirs();
-		Segment mSeg = new Segment("chr6:31380150-31380165");
-		String outfile = outDir + ext.rootOf(vcf) + ".mica.matrix.txt";
-		PrintWriter writer = Files.getAppropriateWriter(outfile);
-		Logger log = new Logger(outDir + "log.log");
-		VCFFileReader reader = new VCFFileReader(new File(vcf), false);
-		String[] samples = VCFOps.getSamplesInFile(reader);
-		boolean[] samplesWithOneGenotype = Array.booleanArray(samples.length, false);
-		writer.println("CHR\tPOS\tREF\tFULL_ALT\tSAMPLE\tA1\tA2\tGQ\tAD");
-		for (VariantContext vc : reader) {
-			if (VCOps.getSegment(vc).overlaps(mSeg)) {
+	private static void run(String[] vcfs, String outDir) {
 
-				if (vc.isIndel()) {
-					int index = 0;
-					StringBuilder builder = new StringBuilder(vc.getContig() + "\t" + vc.getStart() + "\t"
-							+ vc.getReference().getBaseString() + "\t" + vc.getAlternateAlleles().toString());
-					for (Genotype g : vc.getGenotypes()) {
+		for (int i = 0; i < vcfs.length; i++) {
+			String vcf = vcfs[i];
+			
+			new File(outDir).mkdirs();
+			Segment mSeg = new Segment("chr6:31380150-31380165");
+			String outfile = outDir + ext.rootOf(vcf) + "_"
+					+ ext.replaceWithLinuxSafeCharacters(mSeg.getUCSClocation(), true) + ".matrix.txt";
+			PrintWriter writer = Files.getAppropriateWriter(outfile);
+			Logger log = new Logger(outDir + "log.log");
+			VCFFileReader reader = new VCFFileReader(new File(vcf), false);
+			String[] samples = VCFOps.getSamplesInFile(reader);
+			boolean[] samplesWithOneGenotype = Array.booleanArray(samples.length, false);
+			writer.println("CHR\tPOS\tREF\tFULL_ALT\tSAMPLE\tA1\tA2\tGQ\tAD\tFullGeno");
+			for (VariantContext vc : reader) {
+				if (VCOps.getSegment(vc).overlaps(mSeg)) {
 
-						if (!g.getSampleName().equals(samples[index])) {
-							writer.close();
+					if (vc.isIndel()) {
+						int index = 0;
+						StringBuilder builder = new StringBuilder(vc.getContig() + "\t" + vc.getStart() + "\t"
+								+ vc.getReference().getBaseString() + "\t" + vc.getAlternateAlleles().toString());
+						for (Genotype g : vc.getGenotypes()) {
 
-							throw new IllegalStateException("Sample mismatch");
-						}
-						StringBuilder builder2 = new StringBuilder(builder.toString());
-						builder2.append("\t" + g.getSampleName());
+							if (!g.getSampleName().equals(samples[index])) {
+								writer.close();
 
-						if (g.isCalled()) {
-							samplesWithOneGenotype[index] = true;
-
-							ArrayList<String> alts = new ArrayList<String>();
-							String ref = null;
-							for (Allele a : g.getAlleles()) {
-								if (a.isNonReference()) {
-									alts.add(a.getBaseString());
-								} else {
-									ref = a.getBaseString();
-								}
-
+								throw new IllegalStateException("Sample mismatch");
 							}
-							if (ref != null) {
-								if (alts.isEmpty()) {
-									builder2.append("\t" + ref + "\t" + ref);
+							StringBuilder builder2 = new StringBuilder(builder.toString());
+							builder2.append("\t" + g.getSampleName());
+
+							if (g.isCalled()) {
+								samplesWithOneGenotype[index] = true;
+
+								ArrayList<String> alts = new ArrayList<String>();
+								String ref = null;
+								for (Allele a : g.getAlleles()) {
+									if (a.isNonReference()) {
+										alts.add(a.getBaseString());
+									} else {
+										ref = a.getBaseString();
+									}
+
+								}
+								if (ref != null) {
+									if (alts.isEmpty()) {
+										builder2.append("\t" + ref + "\t" + ref);
+									} else {
+										builder2.append("\t" + ref + "\t" + alts.get(0));
+										if (alts.size() != 1) {
+											writer.close();
+
+											throw new IllegalStateException("geno mismatch");
+										}
+									}
 								} else {
-									builder2.append("\t" + ref + "\t" + alts.get(0));
-									if (alts.size() != 1) {
+									if (alts.size() != 2) {
 										writer.close();
 
 										throw new IllegalStateException("geno mismatch");
+
+									}
+									for (String al : alts) {
+										builder2.append("\t" + al);
 									}
 								}
 							} else {
-								if (alts.size() != 2) {
-									writer.close();
-
-									throw new IllegalStateException("geno mismatch");
-
-								}
-								for (String al : alts) {
-									builder2.append("\t" + al);
-								}
+								builder2.append("\t.\t.");
 							}
-						} else {
-							builder2.append("\t.\t.");
+							builder2.append("\t" + g.getGQ() + "\t" + (g.getAD() == null ? g.getAnyAttribute("DPR")
+									: Array.toStr(Array.toStringArray((g.getAD())), ",")));
+							builder2.append(g.toString());
+							if (!g.getSampleName().contains("H20-BLANK")) {
+								writer.println(builder2.toString());
+							}
+							index++;
 						}
-						builder2.append("\t" + g.getGQ() + "\t" + (g.getAD() == null ? g.getAnyAttribute("DPR")
-								: Array.toStr(Array.toStringArray((g.getAD())), ",")));
-
-						if (!g.getSampleName().contains("H20-BLANK")) {
-							writer.println(builder2.toString());
-						}
-						index++;
 					}
 				}
 			}
+			writer.close();
+			reader.close();
+			log.reportTimeInfo(
+					Array.booleanArraySum(samplesWithOneGenotype) + " of " + samples.length + " samples had genotypes");
 		}
-		writer.close();
-		reader.close();
-		log.reportTimeInfo(
-				Array.booleanArraySum(samplesWithOneGenotype) + " of " + samples.length + " samples had genotypes");
 
 	}
 
 	public static void main(String[] args) {
 		CLI c = new CLI(ABLookup.class);
-		c.addArgWithDefault("vcf", "vcf file to explore", null);
+		c.addArgWithDefault("vcfs", "vcf files to explore,comma-delimited list", null);
 		c.addArgWithDefault("outDir", "output directory", null);
 		c.parseWithExit(args);
-		run(c.get("vcf"), c.get("outDir"));
+		run(c.get("vcfs").split(","), c.get("outDir"));
 	}
 
 }
