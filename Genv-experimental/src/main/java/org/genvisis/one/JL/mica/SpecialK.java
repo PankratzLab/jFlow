c6bb01d2e662c3539acee66ae5acc6025c365656
jlanej <jlanej@gmail.com>
2016-10-28 16:54:19 -0500

    SpecialK: Upstream and downstream special variant counts

--- Genv-experimental/src/main/java/org/genvisis/one/JL/mica/SpecialK.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/mica/SpecialK.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.genvisis.CLI;
 import org.genvisis.common.Array;
@@ -28,14 +29,35 @@ import htsjdk.samtools.ValidationStringency;
  */
 public class SpecialK {
 
+	private static final String OTHER = "OTHER";
+	private static final String TOTAL = "TOTAL";
+
 	private static final String STR = "GCT";
 	private static final String START = "TGCTGTT";
 	private static final String END = "ATTTTT";
-
 	private static final String G_INSERT = "G";
 
-	private static HashMap<String, Integer> getCountMap() {
-		HashMap<String, Integer> countMap = new HashMap<String, Integer>();
+	private static final String UPSTREAM_INSERTION = "CAAGTCCCTTTTTTTTCAGG";
+	private static final String DOWNSTREAM_VARIANT = "TTTTCTACGTCTGTTGTTGT";
+
+	private static final String AND = "&&";
+
+	private static class Kmers {
+		private HashMap<String, Integer> countMap;
+		private HashMap<String, String> key;
+
+		public Kmers(HashMap<String, Integer> countMap, HashMap<String, String> key) {
+			super();
+			this.countMap = countMap;
+			this.key = key;
+		}
+
+	}
+
+	private static Kmers getCountMap() {
+		HashMap<String, Integer> countMap = new HashMap<>();
+		HashMap<String, String> key = new HashMap<>();
+
 		for (int i = 4; i < 11; i++) {
 			StringBuilder repeat = new StringBuilder();
 			StringBuilder repeatRef = new StringBuilder();
@@ -55,11 +77,35 @@ public class SpecialK {
 			repeat.append(END);
 			repeatRef.append(END);
 			if (addRef) {
+				key.put(repeatRef.toString(), "A5.1");
+				key.put(repeatRef.toString() + AND + UPSTREAM_INSERTION, "A5.1_UPSTREAM_INSERTION");
+				key.put(repeatRef.toString() + AND + DOWNSTREAM_VARIANT, "A5.1_DOWNSTREAM_VARIANT");
+				key.put(repeatRef.toString() + AND + UPSTREAM_INSERTION + AND + DOWNSTREAM_VARIANT,
+						"A5.1_UPSTREAM_INSERTION_DOWNSTREAM_VARIANT");
+
 				countMap.put(repeatRef.toString(), 0);
+				countMap.put(repeatRef.toString() + AND + UPSTREAM_INSERTION, 0);
+				countMap.put(repeatRef.toString() + AND + DOWNSTREAM_VARIANT, 0);
+				countMap.put(repeatRef.toString() + AND + UPSTREAM_INSERTION + AND + DOWNSTREAM_VARIANT, 0);
 			}
 			countMap.put(repeat.toString(), 0);
+			countMap.put(repeat.toString() + AND + UPSTREAM_INSERTION, 0);
+			countMap.put(repeat.toString() + AND + DOWNSTREAM_VARIANT, 0);
+			countMap.put(repeat.toString() + AND + UPSTREAM_INSERTION + AND + DOWNSTREAM_VARIANT, 0);
+
+			key.put(repeat.toString(), "A" + i);
+			key.put(repeat.toString() + AND + UPSTREAM_INSERTION, "A" + i + "_UPSTREAM_INSERTION");
+			key.put(repeat.toString() + AND + DOWNSTREAM_VARIANT, "A" + i + "_DOWNSTREAM_VARIANT");
+			key.put(repeat.toString() + AND + UPSTREAM_INSERTION + AND + DOWNSTREAM_VARIANT,
+					"A" + i + "_UPSTREAM_INSERTION_DOWNSTREAM_VARIANT");
 		}
-		return countMap;
+
+		countMap.put(TOTAL, 0);
+		key.put(TOTAL, TOTAL);
+		countMap.put(OTHER, 0);
+		key.put(OTHER, OTHER);
+
+		return new Kmers(countMap, key);
 	}
 
 	private static void run(String bamDir, String outDir, String mergeFile) {
@@ -68,27 +114,38 @@ public class SpecialK {
 		Logger log = new Logger(outDir + "sk.log");
 		String[] bams = Files.listFullPaths(bamDir, ".bam", false);
 		log.reportTimeInfo("Found " + bams.length + " bams");
-		Segment loc = new Segment("chr6:31,380,133-31,380,188");
+		Segment loc = new Segment("chr6:31,380,082-31,380,305");
+		HashSet<String> upSDs = new HashSet<>();
+
+		upSDs.add(DOWNSTREAM_VARIANT);
+		upSDs.add(UPSTREAM_INSERTION);
+
+		Kmers kmers = getCountMap();
 		ArrayList<String> allRepeats = new ArrayList<>();
-		allRepeats.addAll(getCountMap().keySet());
+		allRepeats.addAll(kmers.countMap.keySet());
 		StringBuilder results = new StringBuilder();
 		StringBuilder resultsBasic = new StringBuilder();
 
 		Collections.sort(allRepeats);
 		results.append("SAMPLE");
-		for (String repeat : allRepeats) {
-			results.append("\t" + repeat);
-			resultsBasic.append("\t" + repeat);
+		resultsBasic.append("SAMPLE");
 
+		for (String repeat : allRepeats) {
+			results.append("\t" + kmers.key.get(repeat) + "|" + repeat);
+			resultsBasic.append("\t" + kmers.key.get(repeat) + "|" + repeat);
 		}
 		for (int i = 0; i < 3; i++) {
 			results.append("\t" + toMerge.get("SAMPLE"));
 		}
-		results.append("\n");
-		resultsBasic.append("\n");
+		results.append("\tOtherCounts\n");
+		resultsBasic.append("\tOtherCounts\n");
 
 		for (int i = 0; i < bams.length; i++) {
-			HashMap<String, Integer> countMap = getCountMap();
+			// if (i > 10) {
+			// break;
+			// }
+			int countOther = 0;
+			HashMap<String, Integer> countMapAll = getCountMap().countMap;
 			SamReader reader = BamOps.getDefaultReader(bams[i], ValidationStringency.STRICT);
 			QueryInterval[] queryInterestIntervals = BamOps.convertSegsToQI(new Segment[] { loc },
 					reader.getFileHeader(), 0, true, true, log);
@@ -98,23 +155,37 @@ public class SpecialK {
 				SAMRecord samRecord = sIterator.next();
 				if (samRecord.getAlignmentStart() < loc.getStart() && samRecord.getAlignmentEnd() > loc.getStop()) {
 					String seq = samRecord.getReadString();
-					for (String repeat : countMap.keySet()) {
-						if (seq.toUpperCase().contains(repeat)) {
-							countMap.put(repeat, countMap.get(repeat) + 1);
+					boolean found = false;
+					for (String repeats : countMapAll.keySet()) {
+						String[] combos = repeats.split(AND);
+						boolean foundAll = true;
+						for (String repeat : combos) {
+							if (!seq.toUpperCase().contains(repeat) && !repeat.equals(OTHER)) {
+								foundAll = false;
+							}
 						}
+						if (foundAll) {
+							countMapAll.put(repeats, countMapAll.get(repeats) + 1);
+							found = true;
+						}
+					}
+					if (!found) {
+						countMapAll.put(OTHER, countMapAll.get(OTHER) + 1);
 					}
 				}
 			}
 			sIterator.close();
 			String samp = BamOps.getSampleName(bams[i], log);
 			results.append(samp);
+			resultsBasic.append(samp);
+
 			for (String repeat : allRepeats) {
-				results.append("\t" + countMap.get(repeat));
-				resultsBasic.append("\t" + countMap.get(repeat));
+				results.append("\t" + countMapAll.get(repeat));
+				resultsBasic.append("\t" + countMapAll.get(repeat));
 
 			}
-			results.append("\t" + toMerge.get(samp) + "\n");
-			resultsBasic.append("\n");
+			results.append("\t" + toMerge.get(samp) + "\t" + countOther + "\n");
+			resultsBasic.append("\t" + countOther + "\n");
 
 			try {
 				reader.close();
