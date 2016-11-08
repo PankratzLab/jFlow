8eb7dd0616c2809b2b4b53cc490d6514c5f2fb92
jlanej <jlanej@gmail.com>
2016-11-08 17:28:24 -0600

    Singeltons: De-prettify

--- Genvisis/src/main/java/org/genvisis/seq/analysis/Singletons.java
+++ Genvisis/src/main/java/org/genvisis/seq/analysis/Singletons.java
@@ -3,6 +3,7 @@ package org.genvisis.seq.analysis;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import org.genvisis.CLI;
@@ -23,69 +24,81 @@ import htsjdk.variant.vcf.VCFFileReader;
 public class Singletons {
 
 
-	private static void detectSingletons(	String vcf, String outDir, int gqThreshold,
-																				List<String> dotAnnos) {
+	private static void detectSingletons(	String[] vcfs, String outDir, int gqThreshold,
+																				List<String> dotAnnos, HashSet<String> sampsToExclude) {
 		new File(outDir).mkdirs();
 		Logger log = new Logger(outDir + "singleton.log");
-		VCFFileReader reader = new VCFFileReader(new File(vcf), true);
-		HashMap<String, Integer> singletonCounts = new HashMap<String, Integer>();
-		String[] samples = VCFOps.getSamplesInFile(vcf);
-		for (int i = 0; i < samples.length; i++) {
-			singletonCounts.put(samples[i], 0);
-		}
-		int num = 0;
-		for (VariantContext vc : reader) {
-			num++;
-			if (num % 10000 == 0) {
-				log.reportTimeInfo("Scanned " + num + " variants");
+		for (String vcf : vcfs) {
+			VCFFileReader reader = new VCFFileReader(new File(vcf), true);
+			HashMap<String, Integer> singletonCounts = new HashMap<String, Integer>();
+			String[] samples = VCFOps.getSamplesInFile(vcf);
+			for (int i = 0; i < samples.length; i++) {
+				if (!sampsToExclude.contains(samples[i])) {
+					singletonCounts.put(samples[i], 0);
+				}
 			}
-			if (!vc.isIndel() && !vc.isFiltered() && vc.getHetCount() + vc.getHomVarCount() == 1) {// singleton
-																																															// in
-																																															// VCF
-				boolean found = false;
-				for (Genotype g : vc.getGenotypes()) {
-					if (g.isHet() || g.isHomVar()) {
-						if (found) {
-							reader.close();
-							throw new IllegalStateException("Invalid singleton");
-						} else {
-							found = true;
-							if (g.getGQ() > gqThreshold) {
-								boolean externalSingleton = true;
-								for (String dot : dotAnnos) {
-									String anno = VCOps.getAnnotationsFor(new String[] {dot}, vc, ".")[0];
-									if (!".".equals(anno)) {
-										externalSingleton = false;
+			int num = 0;
+			for (VariantContext vcRaw : reader) {
+				VariantContext vc = VCOps.getSubset(vcRaw, singletonCounts.keySet());
+				num++;
+				if (num % 10000 == 0) {
+					log.reportTimeInfo("Scanned " + num + " variants");
+				}
+				if (!vc.isIndel() && !vc.isFiltered() && vc.getHetCount() + vc.getHomVarCount() == 1) {// singleton
+																																																// in
+																																																// VCF
+					boolean found = false;
+					for (Genotype g : vc.getGenotypes()) {
+						if (g.isHet() || g.isHomVar()) {
+							if (found) {
+								reader.close();
+								throw new IllegalStateException("Invalid singleton");
+							} else {
+								found = true;
+								if (g.getGQ() > gqThreshold) {
+									boolean externalSingleton = true;
+									for (String dot : dotAnnos) {
+										String anno = VCOps.getAnnotationsFor(new String[] {dot}, vc, ".")[0];
+										if (!".".equals(anno)) {
+											externalSingleton = false;
+										}
 									}
-								}
-								if (externalSingleton) {
-									singletonCounts.put(g.getSampleName(),
-																			singletonCounts.get(g.getSampleName()) + 1);
+									if (externalSingleton) {
+										singletonCounts.put(g.getSampleName(),
+																				singletonCounts.get(g.getSampleName()) + 1);
+									}
 								}
 							}
 						}
 					}
 				}
 			}
-		}
-		reader.close();
+			reader.close();
 
-		String outFile = outDir + "Singleton.counts_GQ_" + gqThreshold + ".txt";
-		for (String anno : dotAnnos) {
-			outFile = ext.addToRoot(outFile, anno);
+			String outFile = outDir+ ext.rootOf(vcf) + "_singleton.counts_GQ_" + gqThreshold
+												+ "_sampleRemoved_" + sampsToExclude.size() + ".txt";
+			for (String anno : dotAnnos) {
+				outFile = ext.addToRoot(outFile, anno);
+			}
+			StringBuilder buildr = new StringBuilder();
+			buildr.append("SAMPLE\tSINGLETONS\n");
+			for (String sample : singletonCounts.keySet()) {
+				buildr.append(sample + "\t" + singletonCounts.get(sample) + "\n");
+			}
+			Files.write(buildr.toString(), outFile);
 		}
-		StringBuilder buildr = new StringBuilder();
-		buildr.append("SAMPLE\tSINGLETONS\n");
-		for (String sample : singletonCounts.keySet()) {
-			buildr.append(sample + "\t" + singletonCounts.get(sample) + "\n");
-		}
-		Files.write(buildr.toString(), outFile);
 	}
 
 	public static void main(String[] args) {
 		CLI c = new CLI(Singletons.class);
 		ArrayList<String> dotAnnos = new ArrayList<String>();
-		dotAnnos.add("PopFreqMax");
+		dotAnnos.add("esp6500si_all");
+		dotAnnos.add("ExAC_ALL");
+		dotAnnos.add("charge.n_blacks");
+		dotAnnos.add("charge.n_whites");
+
+		HashSet<String> sampsToExclude = new HashSet<String>();
+		sampsToExclude.add("HapMapP_28");
 		c.addArgWithDefault("vcf", "vcf to annotate with default methods", "a.vcf");
 		c.addArgWithDefault("outDir", "output directory", "out/");
 		c.addArgWithDefault("gq", "GQ threshold", "20");
@@ -93,7 +106,10 @@ public class Singletons {
 
 		c.parseWithExit(args);
 		for (int i = 20; i < 80; i += 40) {
-			detectSingletons(c.get("vcf"), c.get("outDir"), i, dotAnnos);
+			detectSingletons(c.get("vcf").split(","), c.get("outDir"), i, dotAnnos, sampsToExclude);
+			detectSingletons(	c.get("vcf").split(","), c.get("outDir"), i, dotAnnos,
+												new HashSet<String>());
+
 		}
 	}
 }
