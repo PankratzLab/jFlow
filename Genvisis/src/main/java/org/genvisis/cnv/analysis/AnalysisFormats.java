68e0dd443a8ae362b79f7d84e02f07a370ee9d72
Mark Hiner <hinerm@gmail.com>
2016-11-08 11:46:24 -0600

    Do sex centroids for penncnv

--- Genvisis/src/main/java/org/genvisis/cnv/analysis/AnalysisFormats.java
+++ Genvisis/src/main/java/org/genvisis/cnv/analysis/AnalysisFormats.java
@@ -190,9 +190,9 @@ public class AnalysisFormats implements Runnable {
 		}
 
 		// Create the scripts for building cnvs from the penncnv data
-		// TODO: sex checks (3rd flag) not done here. Should probably determine if they would be
+		// TODO: sex checks (3rd flag) - should probably determine if they would be
 		// appropriate or not.
-		PennCNV.doBatch(proj, true, true, false, false, 1, true, null, null, null, false, true, false,
+		PennCNV.doBatch(proj, true, true, true, false, 1, true, null, null, null, false, true, false,
 										threadCount);
 	}
 
