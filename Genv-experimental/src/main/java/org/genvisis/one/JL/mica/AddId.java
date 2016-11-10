6907467a99015ede336bc0b95a0aca5821d758d1
jlanej <jlanej@gmail.com>
2016-11-10 14:23:13 -0600

    AddId: a quick script for adding adds

---
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/mica/AddId.java
@@ -0,0 +1,42 @@
+package org.genvisis.one.JL.mica;
+
+import java.io.File;
+
+import org.genvisis.common.Logger;
+import org.genvisis.seq.manage.VCFOps;
+import org.genvisis.seq.manage.VCOps;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.VariantContextBuilder;
+import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+import htsjdk.variant.vcf.VCFFileReader;
+
+public class AddId {
+	private AddId() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String vcfFile = "/Volumes/Beta/data/MICA/data/plink/gatk.mica.nodDup.chr6.mica.targets.hg19_multianno.eff.vcf";
+		VCFOps.verifyIndex(vcfFile, new Logger());
+		String out = VCFOps.getAppropriateRoot(vcfFile, false) + ".ids.vcf";
+		VCFFileReader reader = new VCFFileReader(new File(vcfFile), true);
+		VariantContextWriter writer = VCFOps.initWriterWithHeader(reader, out, VCFOps.DEFUALT_WRITER_OPTIONS,
+				new Logger());
+		for (VariantContext vc : reader) {
+			String id = VCOps.getAnnotationsFor(new String[] { "snp138" }, vc, ".")[0];
+			if (".".equals(id)) {
+				id = new VCOps.LocusID(vc).getId();
+			}
+			VariantContextBuilder builder = new VariantContextBuilder(vc);
+			builder.id(id + "_" + VCOps.getAnnotationsFor(new String[] { "EFF" }, vc, ".")[0].split("\\(")[0]);
+			writer.add(builder.make());
+		}
+		reader.close();
+		writer.close();
+
+	}
+}
