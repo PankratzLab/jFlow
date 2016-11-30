c0d43adbac60b2f717befdf21b0fe8dbabc19a20
Mark Hiner <hinerm@gmail.com>
2016-11-30 10:50:44 -0600

    Fix single-column Unique calc
    The sort method was mangling the values, causing the first unique value
    for a given count to be repeated for each entry that had that count.

--- Genvisis/src/main/java/org/genvisis/common/Unique.java
+++ Genvisis/src/main/java/org/genvisis/common/Unique.java
@@ -6,8 +6,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Vector;
@@ -176,68 +180,43 @@ public class Unique {
 	}
 
 	public static String proc(String[] array, boolean sorted) {
-		Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
-		StringBuilder stringBuilder;
-		String ls, temp;
-		int count;
+		Map<String, KeyCounts> map = new HashMap<String, KeyCounts>();
+		StringBuilder sb;
+		String ls;
 
-		temp = null;
-		count = 0;
 		for (int i = 0; i < array.length; i++) {
-			if (!hash.containsKey(array[i])) {
-				hash.put(array[i], 1);
+			String value = array[i];
+			KeyCounts e = map.get(array[i]);
+			if (e == null) {
+				map.put(value, new KeyCounts(value));
 			} else {
-				hash.put(array[i], hash.get(array[i]) + 1);
-			}
-			count++;
-		} ;
-
-		System.out.println("Found "	+ hash.size() + " unique records among " + count
-												+ " total records");
-		stringBuilder = new StringBuilder();
-		ls = System.getProperty("line.separator");
-		stringBuilder.append(hash.keySet().size());
-		stringBuilder.append(ls);
-		stringBuilder.append(ls);
-
-		if (sorted) {
-			String[] aSort = sortKeysByCount(hash);
-			for (String element : aSort) {
-				stringBuilder.append(element);
-				stringBuilder.append(ls);
-			}
-
-		} else {
-			for (Entry<String, Integer> e : hash.entrySet()) {
-				stringBuilder.append(e.getKey() + "\t" + e.getValue());
-				stringBuilder.append(ls);
+				e.increment();
 			}
 		}
-		temp = stringBuilder.toString();
+
+		System.out.println("Found "	+ map.size() + " unique records among " + array.length
+												+ " total records");
+		sb = new StringBuilder();
+		ls = System.getProperty("line.separator");
+		sb.append(map.size());
+		sb.append(" unique values");
+		sb.append(ls);
+		sb.append(ls);
+
+		Collection<KeyCounts> values = map.values();
+		if (sorted) {
+			List<KeyCounts> valList = new ArrayList<KeyCounts>(values);
+			Collections.sort(valList);
+			values = valList;
+		}
+
+		for (KeyCounts e : values) {
+			sb.append(e.key + "\t" + e.count);
+			sb.append(ls);
+		}
 
 		System.out.println("...done");
-		return temp;
-	}
-
-	/**
-	 * @param hash Map of keys to their counts
-	 * @return String[] of (key\tcount) entries, ordered by decreasing counts
-	 */
-	private static String[] sortKeysByCount(final Hashtable<String, Integer> hash) {
-		int[] counts = new int[hash.keySet().size()];
-		Map<Integer, String> keycounts = new HashMap<Integer, String>();
-		int index = 0;
-		for (Entry<String, Integer> e : hash.entrySet()) {
-			counts[index] = e.getValue();
-			keycounts.put(e.getValue(), e.getKey() + "\t" + e.getValue());
-			index++;
-		}
-		Sort.reverseSort(counts);
-		String[] sorted = new String[counts.length];
-		for (int i = 0; i < sorted.length; i++) {
-			sorted[i] = keycounts.get(counts[i]);
-		}
-		return sorted;
+		return sb.toString();
 	}
 
 	public static String[][] proc(String[][] arrays, boolean verbose) {
@@ -279,6 +258,45 @@ public class Unique {
 		}
 
 		return allCounts;
+	}
+
+	private static class KeyCounts implements Comparable<KeyCounts> {
+
+		private final String key;
+		private Integer count;
+
+		public KeyCounts(String k) {
+			key = k;
+			count = 1;
+		}
+
+		public void increment() {
+			count++;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+
+			if (o instanceof KeyCounts) {
+				KeyCounts other = (KeyCounts)o;
+				return key.equals(other.key) && count.equals(other.count);
+			}
+
+			return false;
+		}
+
+		@Override
+		public int compareTo(KeyCounts o) {
+			// Sort larger counts first
+			int c = o.count.compareTo(count);
+			if (c == 0) {
+				c = o.key.compareTo(key);
+			}
+			return c;
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
