6da520b75f34241cc2e5651fed6308a41beef9a1
jlanej <jlanej@gmail.com>
2016-12-05 19:57:00 -0600

    SkatMtDNA: adding in functional filters and SnpInfo

--- Genv-experimental/src/main/java/org/genvisis/one/JL/mtDNA/SkatMtDNA.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/mtDNA/SkatMtDNA.java
@@ -10,6 +10,7 @@ import org.genvisis.seq.manage.VCFOps;
 import org.genvisis.seq.manage.VCOps;
 
 import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.VariantContextBuilder;
 import htsjdk.variant.variantcontext.writer.VariantContextWriter;
 import htsjdk.variant.vcf.VCFFileReader;
 
@@ -27,27 +28,32 @@ public class SkatMtDNA {
 		for (VariantContext vc : reader) {
 			String annoVc = VCOps.getAnnotationsFor(new String[] { anno }, vc, ".")[0];
 			if (!annoVc.equals(annoFilt)) {
+				VariantContextBuilder builder = new VariantContextBuilder(vc);
+//				builder.id(ID)
 				writer.add(vc);
 			}
 		}
-
 		reader.close();
 		writer.close();
 	}
 
-	private static void filterCR(String inputVCF, double cr, String outDir, String pseqDir, Logger log) {
-
-		// cat i-statsats.mtdna.ALL.txt | grep -v NALT|awk '$6<=.95'>
-		// excludeSamples.txt
-		// vcftools --vcf
-		// ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.vcf
-		// --remove excludeSamples.txt --recode --recode-INFO-all --out
-		// ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.HQ.SAMPS
-		// vcftools --vcf
-		// ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.HQ.SAMPS.recode.vcf
-		// --max-missing $callRate --recode --recode-INFO-all --out
-		// ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.HQ.SAMPS.$callRate.CR
-
+	private static String filterCR(String inputVCF, double cr, String outDir, String pseqDir, Logger log) {
+		String istats = runIstats(inputVCF, pseqDir, outDir, log);
+		StringBuilder filter = new StringBuilder();
+		String root = outDir + VCFOps.getAppropriateRoot(inputVCF, true) + "cr" + cr;
+		String excludes = root + ".excludedSamps.txt";
+		filter.append("cat " + istats + " | grep -v NALT|awk '$6<=" + cr + "'>" + excludes + "\n");
+		String out1 = root + ".HQ.vcf";
+		filter.append("vcftools --vcf " + inputVCF + " --remove excludeSamples.txt --max-missing " + cr
+				+ " --recode --recode-INFO-all --out " + out1);
+		if (!Files.exists(out1)) {
+			String bat = out1 + ".bat";
+			Files.write(filter.toString(), bat);
+			Files.chmod(bat);
+			log.reportTimeInfo(filter.toString());
+			CmdLine.run(bat, outDir);
+		}
+		return out1;
 	}
 
 	private static String runIstats(String vcf, String pseqDir, String outDir, Logger log) {
@@ -58,11 +64,6 @@ public class SkatMtDNA {
 		istatGetter.append(pseqDir + "pseq " + proj + " load-vcf --vcf " + vcf + "\n");
 		istatGetter.append(pseqDir + "pseq " + proj + " i-stats >" + out);
 
-		// "$plinkseqDir"pseq proj new-project
-		// #load vcf
-		// "$plinkseqDir"pseq proj load-vcf --vcf
-		// ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.vcf
-		// "$plinkseqDir"pseq proj i-stats >i-statsats.mtdna.ALL.txt
 		if (!Files.exists(out)) {
 			Files.write(istatGetter.toString(), bat);
 			Files.chmod(bat);
@@ -78,7 +79,7 @@ public class SkatMtDNA {
 		new File(outDir).mkdirs();
 		String inputVCF = "/Volumes/Beta/data/mtDNA-dev/vcf/analysis/ARIC_CUSHING_EPP_OSTEO_FP_MITO.chrM.rcrs.poly.disease.conv.hg19_multianno.eff.gatk.sed1000g.vcf";
 		Logger log = new Logger(outDir + "log.log");
-		runIstats(inputVCF, pseqDir, outDir, log);
+//		runIstats(inputVCF, pseqDir, outDir, log);
 	}
 
 }
