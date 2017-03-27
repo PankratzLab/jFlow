85acabcd72e9bd7afaa582d622f8bea0ad739405
jlanej <jlanej@gmail.com>
2017-03-27 08:58:33 -0500

    Placeholder for threading out experimental association tests for genome
    wide scan

---
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/assoc/AssocTests.java
@@ -0,0 +1,14 @@
+package org.genvisis.one.JL.assoc;
+
+/**
+ * Placeholder for threading out genome wide assoc scan
+ *
+ */
+public class AssocTests {
+
+	// dir with .counts.txt
+	// threads
+	// pheno.txt
+	// out dir
+
+}
