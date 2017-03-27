daa8a1929e6cf904303558b69973b3036d5f77ca
jlanej <jlanej@gmail.com>
2017-03-27 12:25:01 -0500

    One-off burden test organizer

--- Genv-experimental/src/main/java/org/genvisis/one/JL/assoc/AssocTests.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/assoc/AssocTests.java
@@ -1,10 +1,41 @@
 package org.genvisis.one.JL.assoc;
 
+import java.io.File;
+
+import org.genvisis.common.ArrayUtils;
+import org.genvisis.common.Files;
+import org.genvisis.common.HashVec;
+import org.genvisis.common.ext;
+import org.genvisis.qsub.Qsub;
+import org.genvisis.stats.Rscript;
+
 /**
  * Placeholder for threading out genome wide assoc scan
  *
  */
 public class AssocTests {
+
+	public static void main(String[] args) {
+		String dir = "/scratch.global/lanej/burdenQA/full/";
+		String inputsFile = "/scratch.global/lanej/burdenQA/full/SPSS_inputs.txt";
+		String rscript = "/scratch.global/lanej/burdenQA/full/msiBurden.R";
+
+		String[] inputs = Files.listFullPaths(dir, "gene_counts.txt", false);
+		String rFile = ArrayUtils.toStr(HashVec.loadFileToStringArray(rscript, false, false, null, false), "\n");
+
+		for (String input : inputs) {
+			String tmp = rFile;
+			tmp = tmp.replaceAll("inputsFile = REPLACE",
+					"inputsFile = read.delim(\"" + inputsFile + "\",stringsAsFactors = FALSE)");
+			tmp = tmp.replaceAll("BurdenCountAllAllFilesAll = REPLACE",
+					"BurdenCountAllAllFilesAll =" + Rscript.generateRVector(new String[] { input }, true));
+
+			String rscriptTmp = ext.addToRoot(input, ".rscript");
+			String qsub = ext.addToRoot(input, ".pbs");
+			Files.write(tmp, rscriptTmp);
+			Qsub.qsub(qsub, "Rscript " + rscriptTmp, 15000, 124, 1);
+		}
+	}
 
 	// dir with .counts.txt
 	// threads
