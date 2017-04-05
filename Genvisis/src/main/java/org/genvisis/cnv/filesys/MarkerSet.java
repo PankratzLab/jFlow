aeb5e93d83d19134af4d980e90bbd7ee1bf8ea28
Spencer Kelley <kelle881@umn.edu>
2017-04-05 16:35:51 -0500

    MarkerSet.load() should return a MarkerSet

--- Genvisis/src/main/java/org/genvisis/cnv/filesys/MarkerSet.java
+++ Genvisis/src/main/java/org/genvisis/cnv/filesys/MarkerSet.java
@@ -190,8 +190,8 @@ public class MarkerSet implements Serializable, TextExport, MarkerSetInfo {
 		SerializedFiles.writeSerial(this, filename);
 	}
 
-	public static MarkerSetInfo load(String filename, boolean jar) {
-		return (MarkerSetInfo) SerializedFiles.readSerial(filename, jar, true);
+	public static MarkerSet load(String filename, boolean jar) {
+		return (MarkerSet) SerializedFiles.readSerial(filename, jar, true);
 	}
 
 	public static long fingerprint(String[] names) {
