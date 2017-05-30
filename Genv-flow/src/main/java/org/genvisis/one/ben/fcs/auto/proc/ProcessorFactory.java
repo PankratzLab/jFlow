7efdd9dfa2997713b3d3ece727d8ade73ed6daf3
cole0482 <cole0482@umn.edu>
2017-05-30 13:41:03 -0500

    Refactor FCS processing pipeline classes to new files;

---
+++ Genv-flow/src/main/java/org/genvisis/one/ben/fcs/auto/proc/ProcessorFactory.java
@@ -0,0 +1,7 @@
+package org.genvisis.one.ben.fcs.auto.proc;
+
+public interface ProcessorFactory<T extends SampleProcessor> {
+	T createProcessor(Object owner, int index);
+
+	void cleanup(Object owner);
+}
