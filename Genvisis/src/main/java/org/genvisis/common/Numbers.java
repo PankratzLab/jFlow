f39c7f19b359aff65789d655e5c10113fbc74909
Mark Hiner <hinerm@gmail.com>
2017-06-13 08:43:10 -0500

    Add a parse signature which accepts a Locale
    In case US is ever not appropriate.

--- Genvisis/src/main/java/org/genvisis/common/Numbers.java
+++ Genvisis/src/main/java/org/genvisis/common/Numbers.java
@@ -14,9 +14,21 @@ public final class Numbers {
 		// prevent instantiation of static utility class
 	}
 
+	/**
+	 * See {@link #parseWithLocale(String, Locale)}. Uses {@link Locale#US}.
+	 */
 	public static int parseWithLocale(String n) {
+		return parseWithLocale(n, Locale.US);
+	}
+
+	/**
+	 * @param n Number string
+	 * @param l {@link Locale} to use when parsing
+	 * @return Integer value of the parsed number
+	 */
+	public static int parseWithLocale(String n, Locale l) {
 		try {
-			return NumberFormat.getInstance(Locale.US).parse(n).intValue();
+			return NumberFormat.getInstance(l).parse(n).intValue();
 		} catch (ParseException e) {
 			throw new NumberFormatException(e.getMessage());
 		}
