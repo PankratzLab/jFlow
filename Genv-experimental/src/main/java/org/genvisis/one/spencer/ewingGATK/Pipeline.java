d5e444a6611edf583774ee4061235a61dfb17de5
Spencer Kelley <kelle881@umn.edu>
2017-06-30 13:53:29 -0500

    More ES one-off pipeline changes

--- Genv-experimental/src/main/java/org/genvisis/one/spencer/ewingGATK/Pipeline.java
+++ Genv-experimental/src/main/java/org/genvisis/one/spencer/ewingGATK/Pipeline.java
@@ -10,8 +10,13 @@ import java.util.stream.Collectors;
 
 import org.genvisis.common.Files;
 import org.genvisis.common.HashVec;
+import org.genvisis.common.Logger;
 import org.genvisis.common.ext;
 import org.genvisis.qsub.Qsub;
+import org.genvisis.seq.analysis.GATK;
+import org.genvisis.seq.analysis.GATK.RESOURCE;
+import org.genvisis.seq.analysis.GATK.SEQ_TARGET;
+import org.genvisis.seq.analysis.RegNovo;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
@@ -32,9 +37,20 @@ public class Pipeline {
 	private static final int SMALL_CORES = 24;
 	private static final int SMALL_MEM_GB = 62;
 
-	private static final Joiner filenameJoiner = Joiner.on("_");
+	private static final Joiner FILENAME_JOINER = Joiner.on("_");
 
-	private static final String REF_GENO = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/WUSTL_RefGenome/all_sequences.fa";
+	private static final String BASE_DIR = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/";
+	private static final String REF_GENO = BASE_DIR + "WUSTL_RefGenome/all_sequences.fa";
+	private static final String REG_NOVO_DIR = BASE_DIR + "RegNovo/";
+	private static final String VCF_DIR = BASE_DIR + "vcfs/";
+
+	private static final String RECAL_SNP_FILE = VCF_DIR + "ES_recalibrate_SNP.recal";
+	private static final String RECAL_SNP_TRANCHES_FILE = VCF_DIR + "ES_recalibrate_SNP.tranches";
+	private static final String RECAL_SNP_RSCRIPT = VCF_DIR + "ES_recalibrate_SNP_plots.R";
+
+	private static final String RECAL_INDEL_FILE = VCF_DIR + "ES_recalibrate_INDEL.recal";
+	private static final String RECAL_INDEL_TRANCHES_FILE = VCF_DIR + "ES_recalibrate_INDEL.tranches";
+	private static final String RECAL_INDEL_RSCRIPT = VCF_DIR + "ES_recalibrate_INDEL_plots.R";
 
 	private final String bamDir;
 	private final Set<String> ids;
@@ -67,7 +83,14 @@ public class Pipeline {
 
 		// runHaplotypeCallerItasca();
 
-		runCombineGVCFs();
+		// runCombineGVCFs();
+
+		// runCombinedGVCFJointGenotyping();
+
+		// runRegNovoPreVQSR();
+
+		runVQSR();
+		// runVariantAnnotator();
 
 	}
 
@@ -76,7 +99,7 @@ public class Pipeline {
 		for (String id : ids) {
 			if (!fileExists(bamIndex(id))) {
 				StringJoiner cmd = new StringJoiner("\n");
-				cmd.add(cdCmd());
+				cmd.add(cdBamDir());
 				cmd.add("");
 				cmd.add("java -jar /home/pankrat2/public/bin/picard/picard.jar BuildBamIndex INPUT="
 								+ bam(id));
@@ -96,7 +119,7 @@ public class Pipeline {
 			// if (!fileExists(recal1(id), recalPBS(id))) {
 			if (true) {
 				StringJoiner cmd = new StringJoiner("\n");
-				cmd.add(cdCmd());
+				cmd.add(cdBamDir());
 				cmd.add("");
 				cmd.add("java -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\")
 					 .add("\t-T BaseRecalibrator \\")
@@ -141,7 +164,7 @@ public class Pipeline {
 			// if (!fileExists(callerGvcf(id), callerPBS(id))) {
 			if (!fileExists(callerGvcf(id), callerGvcfIndex(id))) {
 				StringJoiner cmd = new StringJoiner("\n");
-				cmd.add(cdCmd());
+				cmd.add(cdBamDir());
 				cmd.add("");
 				cmd.add("java -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\")
 					 .add("\t-T HaplotypeCaller \\")
@@ -164,7 +187,7 @@ public class Pipeline {
 	private void runHaplotypeCallerItasca() {
 		final int threads = 4;
 		Iterator<String> idIter = ids.iterator();
-		Set<String> itascaIDs = HashVec.loadFileToHashSet("/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/bams/itascaIDs",
+		Set<String> itascaIDs = HashVec.loadFileToHashSet(BASE_DIR + "bams/itascaIDs",
 																											false);
 		Set<String> pbsScripts = Sets.newHashSet();
 		while (idIter.hasNext()) {
@@ -179,7 +202,7 @@ public class Pipeline {
 			}
 
 			StringJoiner cmd = new StringJoiner("\n");
-			cmd.add(cdCmd());
+			cmd.add(cdBamDir());
 			for (String id : idsToBatch) {
 				cmd.add("");
 				cmd.add("java -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\")
@@ -201,7 +224,7 @@ public class Pipeline {
 			pbsScripts.add(QSUB + " -q sb " + pbs);
 		}
 		Files.writeIterable(pbsScripts,
-												"/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/bams/runItascaBatch");
+												BASE_DIR + "bams/runItascaBatch");
 
 	}
 
@@ -214,7 +237,7 @@ public class Pipeline {
 		for (String id : ids) {
 			boolean write = false;
 			StringJoiner cmd = new StringJoiner("\n");
-			cmd.add(cdCmd());
+			cmd.add(cdBamDir());
 			cmd.add("");
 			for (int i = 2; i < 26; i++) {
 
@@ -244,7 +267,7 @@ public class Pipeline {
 				commands.add("qsub " + (cmdCount < 48 ? "-q pankratz " : "") + splitPBS(id));
 				if (cmdCount++ > 400) {
 					Files.writeIterable(commands,
-															"/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/pbsScripts/splitGVCFJobs_"
+															BASE_DIR + "pbsScripts/splitGVCFJobs_"
 																				+ commandScriptCount);
 					commands.clear();
 					commandScriptCount++;
@@ -253,7 +276,7 @@ public class Pipeline {
 			}
 		}
 		Files.writeIterable(commands,
-												"/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/pbsScripts/splitGVCFJobs_"
+												BASE_DIR + "pbsScripts/splitGVCFJobs_"
 																	+ commandScriptCount);
 	}
 
@@ -265,19 +288,19 @@ public class Pipeline {
 
 			String chr = intToChr(i);
 			for (int batch = 1; batch <= 6; batch++) {
-				String gvcfList = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/pbsScripts/chr"
+				String gvcfList = BASE_DIR + "pbsScripts/chr"
 													+ chr + "_gVCFs_batch_" + batch + ".list";
 				// Files.writeIterable(ids.stream().map(id -> splitGvcf(id,
 				// chr)).collect(Collectors.toList()),
 				// gvcfList);
 
-				String pbsScript = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/pbsScripts/CG_chr"
+				String pbsScript = BASE_DIR + "pbsScripts/CG_chr"
 													 + chr + "_gVCFs_batch_" + batch + "_combineGVCFs.pbs";
 
 				if (!fileExists(combinedGVCF(chr, batch), combinedGVCFIndex(chr, batch))) {
 
 					StringJoiner cmd = new StringJoiner("\n");
-					cmd.add(cdCmd());
+					cmd.add(cdBamDir());
 					cmd.add("");
 					cmd.add("java -Xmx14G -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\")
 						 .add("\t-T CombineGVCFs \\")
@@ -310,15 +333,15 @@ public class Pipeline {
 		final int threads = 1;
 		for (int i = 1; i < 26; i++) {
 			StringJoiner cmd = new StringJoiner("\n");
-			cmd.add(cdCmd());
+			cmd.add(cdBamDir());
 			cmd.add("");
 			final String chr = intToChr(i);
 
-			String chrGVCFs = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/chr" + chr
+			String chrGVCFs = BASE_DIR + "chr" + chr
 												+ "_gVCFs.list";
 
 			Files.writeIterable(ids.stream()
-														 .map(s -> "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/gVCFs_byChr/"
+														 .map(s -> BASE_DIR + "gVCFs_byChr/"
 																			 + s + "_chr" + chr + "_recalibrated.snps.indels.g.vcf.gz")
 														 .collect(Collectors.toList()),
 													chrGVCFs);
@@ -338,6 +361,139 @@ public class Pipeline {
 
 	}
 
+	private void runCombinedGVCFJointGenotyping() {
+
+		final int threads = 1;
+		for (int i = 1; i < 26; i++) {
+
+			final String chr = intToChr(i);
+
+			if (!Files.checkAllFiles("", combinedGVCFjointVCF(chr), combinedGVCFjointVCF(chr))) {
+				boolean ready = true;
+				StringJoiner cmd = new StringJoiner("\n");
+				cmd.add(cdBamDir());
+				cmd.add("");
+				cmd.add("");
+				cmd.add("java -Xmx18G -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\")
+					 .add("\t-T GenotypeGVCFs \\")
+					 .add("\t-R " + REF_GENO + " \\");
+				for (int batch = 1; batch <= 6; batch++) {
+					cmd.add("\t-V " + combinedGVCF(chr, batch) + " \\");
+					if (!fileExists(combinedGVCF(chr, batch), combinedGVCFIndex(chr, batch))) {
+						System.err.println(combinedGVCFIndex(chr, batch) + " does not exist");
+						ready = false;
+						break;
+					}
+				}
+				cmd.add("\t-o " + combinedGVCFjointVCF(chr) + " \\")
+					 .add("\t-L " + chr);
+				cmd.add("");
+				if (ready)
+					Qsub.qsubGb(combinedGVCFjointGenoPBS(chr), cmd.toString(), 20, 288, threads);
+			}
+		}
+
+	}
+
+	private void runVQSR() {
+		GATK gatk = new GATK("/home/pankrat2/public/bin/GATK_3.7/", REF_GENO, null,
+												 SEQ_TARGET.GENOME, null, 250000, null, null, true, false,
+												 new Logger("VQSR.log"));
+
+		Map<GATK.RESOURCE, String> trainingResources = Maps.newEnumMap(GATK.RESOURCE.class);
+		String RESOURCE_DIRECTORY = "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/WUSTL_RefGenome/";
+		trainingResources.put(RESOURCE.HAPMAP, RESOURCE_DIRECTORY + "hapmap_3.3.b37.vcf");
+		trainingResources.put(RESOURCE.OMNI, RESOURCE_DIRECTORY + "1000G_omni2.5.b37.vcf");
+		trainingResources.put(RESOURCE.DBSNP, RESOURCE_DIRECTORY + "dbsnp_138.b37.vcf");
+		trainingResources.put(RESOURCE.G1K,
+													RESOURCE_DIRECTORY + "1000G_phase1.snps.high_confidence.b37.vcf");
+		trainingResources.put(RESOURCE.MILLS,
+													RESOURCE_DIRECTORY + "Mills_and_1000G_gold_standard.indels.b37.vcf");
+		gatk.setTrainingResources(trainingResources);
+		int threads = 24;
+		Joiner cmdJoiner = Joiner.on(' ');
+
+		String[] jointGenoVCFs = HashVec.loadFileToStringArray(BASE_DIR + "jointGenotypeVCFs.list",
+																													 false, null, false);
+		StringJoiner cmd = new StringJoiner("\n");
+		cmd.add(cdVCFDirectory());
+		if (!Files.checkAllFiles("", RECAL_SNP_FILE, RECAL_SNP_TRANCHES_FILE, RECAL_SNP_RSCRIPT)) {
+			cmd.add(cmdJoiner.join(gatk.buildSNPRecalibrationModelCommand(BASE_DIR
+																																		+ "jointGenotypeVCFs.list",
+																																		RECAL_SNP_FILE,
+																																		RECAL_SNP_TRANCHES_FILE,
+																																		RECAL_SNP_RSCRIPT, false,
+																																		threads)));
+		}
+		if (!Files.checkAllFiles("", snpRecaledVCF(), snpRecaledVCF() + ".tbi")) {
+			cmd.add("");
+			cmd.add(cmdJoiner.join(gatk.applySNPRecalibrationModelCommand(RECAL_SNP_FILE,
+																																		RECAL_SNP_TRANCHES_FILE,
+																																		snpRecaledVCF(),
+																																		threads,
+																																		jointGenoVCFs)));
+			cmd.add("");
+		}
+		if (!Files.checkAllFiles("", RECAL_INDEL_FILE, RECAL_INDEL_TRANCHES_FILE,
+														 RECAL_INDEL_RSCRIPT)) {
+			cmd.add("");
+			cmd.add(cmdJoiner.join(gatk.buildINDELRecalibrationModelCommand(snpRecaledVCF(),
+																																			RECAL_INDEL_FILE,
+																																			RECAL_INDEL_TRANCHES_FILE,
+																																			RECAL_INDEL_RSCRIPT, false,
+																																			threads)));
+			cmd.add("");
+		}
+		if (!Files.checkAllFiles("", snpIndelRecaledVCF(), snpIndelRecaledVCF() + ".tbi")) {
+			cmd.add("");
+			cmd.add(cmdJoiner.join(gatk.applyINDELRecalibrationModelCommand(RECAL_INDEL_FILE,
+																																			RECAL_INDEL_TRANCHES_FILE,
+																																			snpIndelRecaledVCF(),
+																																			threads,
+																																			snpRecaledVCF())));
+		}
+		if (!cmd.toString().equals(cdVCFDirectory())) {
+			Qsub.qsubGb(BASE_DIR + "VQSR_ES.pbs", cmd.toString(), 252, 175, threads);
+		}
+	}
+
+	private void runVariantAnnotator() {
+		for (int i = 1; i < 26; i++) {
+			String chr = intToChr(i);
+			StringJoiner cmd = new StringJoiner("\n");
+			cmd.add(cdVCFDirectory());
+			cmd.add("");
+			cmd.add("java -Xmx8g -jar /home/pankrat2/public/bin/GATK_3.7/GenomeAnalysisTK.jar \\");
+			cmd.add("-T VariantAnnotator \\");
+			cmd.add("-V " + combinedGVCFjointVCF(chr) + " \\");
+			cmd.add("-R " + REF_GENO + " \\");
+			cmd.add("-A InbreedingCoeff \\");
+			cmd.add("-ped /panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/sampleInfo/ES.ped \\");
+			cmd.add("--dbsnp /panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/WUSTL_RefGenome/dbsnp_138.b37.vcf \\");
+			cmd.add("-o " + combinedGVCFjointVCFExtraAnno(chr));
+
+			Qsub.qsub(BASE_DIR + "pbsScripts/ICA_chr" + chr + "_AnnotateInbreedingCoeff.pbs",
+								cmd.toString(), 10500, 100, 1);
+		}
+	}
+
+	private void runRegNovoPreVQSR() {
+		for (int i = 1; i <= 25; i++) {
+			String chr = intToChr(i);
+			StringJoiner cmd = new StringJoiner("\n");
+			cmd.add(cdRegNovoDir());
+			cmd.add("java -jar ~/genvisis_RegNovo.jar " + RegNovo.class.getName() + " \\");
+			cmd.add("\t" + "vcf=../vcfs/ES_JointGenotypes_chr" + chr + "_combinedGVCFs.vcf.gz \\");
+			cmd.add("\t" + "vpop=ES_Trios.vpop \\");
+			cmd.add("\t" + "regions=chr" + chr + " \\");
+			cmd.add("\t" + "outputRoot=ES_RegNovo_PreVQSR_chr" + chr + " \\");
+			cmd.add("\t" + "log=ES_RegNovo_PreVQSR_chr" + chr + ".log");
+
+			Qsub.qsub(REG_NOVO_DIR + "RNPV_chr" + chr + "_RegNovoPreVQSR.pbs", cmd.toString(), 10500, 200,
+								1);
+		}
+	}
+
 	private String intToChr(int i) {
 		String chr = Integer.toString(i);
 		if (i == 23)
@@ -350,8 +506,16 @@ public class Pipeline {
 	}
 
 
-	private String cdCmd() {
+	private String cdBamDir() {
 		return "cd " + bamDir;
+	}
+
+	private String cdRegNovoDir() {
+		return "cd " + REG_NOVO_DIR;
+	}
+
+	private String cdVCFDirectory() {
+		return "cd " + VCF_DIR;
 	}
 
 	private String bam(String id) {
@@ -387,12 +551,26 @@ public class Pipeline {
 	}
 
 	private String jointVCF(String chr) {
-		return "/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/vcfs/ES_JointGenotypes_chr"
+		return BASE_DIR + "vcfs/ES_JointGenotypes_chr"
 					 + chr + "_splitGVCFs.vcf.gz";
 	}
 
 	private String jointVCFIndex(String chr) {
 		return jointVCF(chr) + ".tbi";
+	}
+
+	private String combinedGVCFjointVCF(String chr) {
+		return BASE_DIR + "vcfs/ES_JointGenotypes_chr"
+					 + chr + "_combinedGVCFs.vcf.gz";
+	}
+
+	private String combinedGVCFjointVCFExtraAnno(String chr) {
+		return BASE_DIR + "vcfs/ES_JointGenotypes_chr"
+					 + chr + "_combinedGVCFs_withPED.vcf.gz";
+	}
+
+	private String combinedGVCFjointVCFIndex(String chr) {
+		return combinedGVCFjointVCF(chr) + ".tbi";
 	}
 
 	private String callerGvcfIndex(String id) {
@@ -423,12 +601,24 @@ public class Pipeline {
 		return "../pbsScripts/J_chr" + chr + "_jointGenotyping" + PBS;
 	}
 
+	private String combinedGVCFjointGenoPBS(String chr) {
+		return "../pbsScripts/JC_chr" + chr + "_combinedGVCFs_jointGenotyping" + PBS;
+	}
+
+	private String snpRecaledVCF() {
+		return VCF_DIR + "ES_recalibrated_snps_raw_indels.vcf.gz";
+	}
+
+	private String snpIndelRecaledVCF() {
+		return VCF_DIR + "ES_recalibrated_snps_indels.vcf.gz";
+	}
+
 	private boolean fileExists(String... filenames) {
 		return Files.exists(bamDir, filenames);
 	}
 
 	public static void main(String[] args) {
-		new Pipeline("/panfs/roc/pankrat2-SpectorEwingSarcoma/SpectorEwingSarcoma/bams/").generatePipeline();
+		new Pipeline(BASE_DIR + "bams/").generatePipeline();
 	}
 
 }
