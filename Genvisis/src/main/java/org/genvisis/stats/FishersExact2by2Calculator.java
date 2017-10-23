aec9ea49712bbe17d6ea82ed8bce17f053860419
Mark Hiner <hinerm@gmail.com>
2017-10-23 14:01:22 -0500

    Staticify FishersExact2by2Calculator
    It is not meaningful to track table counts between individual p-value
    computations, so converting this functionality to a static utility class
    makes it a bit more concicse (removing the need to instantiate in each
    place of use, and always perform a load call followed by a compute
    call).
    
    The state that is helpful is the table of cached log factorial values.
    Converting this to a list allows the ability to keep a single reference
    which can be dynamically expanded as needed, and reused between
    computations.

--- Genvisis/src/main/java/org/genvisis/stats/FishersExact2by2Calculator.java
+++ Genvisis/src/main/java/org/genvisis/stats/FishersExact2by2Calculator.java
@@ -1,147 +1,190 @@
 package org.genvisis.stats;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.genvisis.common.ext;
 
+/**
+ * Utility class for computing
+ * <a href="https://en.wikipedia.org/wiki/Fisher%27s_exact_test">Fisher's exact test</a> on a 2x2
+ * matrix.
+ */
 public class FishersExact2by2Calculator {
-	private double[] logFactorial;
-	private int a;
-	private int b;
-	private int c;
-	private int d;
-	private int n;
-	private int tmp;
-	
-	public FishersExact2by2Calculator() {
-		logFactorial = null;
-
-		this.a = 0;
-    	this.b = 0;
-    	this.c = 0;
-    	this.d = 0;
+	private FishersExact2by2Calculator() {
+		// Prevent instantiation of static utility class
 	}
-	
-    public void loadMatrix(int[][] matrix) {
-    	loadCounts(matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]);
-    }
 
-    public void loadCounts(int a, int b, int c, int d) {
-    	this.a = a;
-    	this.b = b;
-    	this.c = c;
-    	this.d = d;
-    }
-    
-    private double testComputation() {
-        return Math.exp(
-        	(logFactorial[a + b] +
-        	logFactorial[c + d] +
-        	logFactorial[a + c] +
-        	logFactorial[b + d])
-        	-
-        	(logFactorial[a + b + c + d] +
-        	logFactorial[a] +
-        	logFactorial[b] +
-        	logFactorial[c] +
-        	logFactorial[d])
-        );
-    }
-    
-    public double getPvalue() {
-    	return getPvalue(false);
-    }
-    
-    public double getPvalue(boolean oneTailed) {
-        n=a+b+c+d;
+	private static final List<Double> CACHED_LOG_FACTORIAL = new ArrayList<>();
 
-        if (logFactorial == null || logFactorial.length < n+1) {
-            logFactorial = new double[n+1];
-            logFactorial[0] = 0.0;
-            for (int i = 1; i <= n; i++) {
-                logFactorial[i] = logFactorial[i-1] + Math.log(i);
-            }
-        }
-        
-        if (a * d > b * c) {
-        	tmp = a;
-        	a = b;
-        	b = tmp;
+	/**
+	 * @param matrix A 2x2 matrix of counts
+	 * @return The two-tailed p-value for the input 2x2 table
+	 * @see #getPvalue(int[][], boolean)
+	 */
+	public static double getPvalue(int[][] matrix) {
+		return getPvalue(matrix, false);
+	}
 
-        	tmp = c;
-        	c = d;
-        	d = tmp;
-        }
-        if (a > d) {
-        	tmp = a;
-        	a = d;
-        	d = tmp;
-        }
-        if (b > c) {
-        	tmp = b;
-        	b = c;
-        	c = tmp;
-        }
+	/**
+	 * @return The two-tailed p-value for the four counts of a 2x2 table
+	 * @see #getPvalue(int, int, int, int, boolean)
+	 */
+	public static double getPvalue(int a, int b, int c, int d) {
+		return getPvalue(a, b, c, d, false);
+	}
 
-        int original_a = a;
-        double sumP = 0;
+	/**
+	 * @param matrix A 2x2 matrix of counts
+	 * @param oneTailed If true, return value is one-tailed p-value. Otherwise, two-tailed.
+	 * @return The one or two-tailed p-value for the input 2x2 table
+	 * @see #getPvalue(int, int, int, int, boolean)
+	 */
+	public static double getPvalue(int[][] matrix, boolean oneTailed) {
+		if (matrix.length != 2 || matrix[0].length != 2 || matrix[1].length != 2) {
+			throw new IllegalArgumentException(FishersExact2by2Calculator.class.getName()
+																				 + " requires a 2x2 input matrix to compute a p-value");
+		}
 
-        double p = testComputation();
-        double p_1 = p;
+		return getPvalue(matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1], oneTailed);
+	}
 
-        while (a >= 0) {
-            sumP += p;
-            if (a == 0) {
-            	break;
-            }
-            a--;
-            b++;
-            c++;
-            d--;
-            p = testComputation();
-        }
-        if (oneTailed) {
-        	return Math.min(sumP, 1);
-        }
+	/**
+	 * @param oneTailed If true, return value is one-tailed p-value. Otherwise, two-tailed.
+	 * @return The one or two-tailed p-value for the four counts of a 2x2 table
+	 */
+	public static double getPvalue(int a, int b, int c, int d, boolean oneTailed) {
+		ensureCacheSize(a, b, c, d);
 
-        a = b;
-        b = 0;
-        c = c - a;
-        d = d + a;
-        p = testComputation();
+		int tmp;
 
-        while (p < p_1) {
-            if (a == original_a) {
-            	break;
-            }
-            sumP += p;
-            a--;
-            b++;
-            c++;
-            d--;
-            p = testComputation();
-        }
-    	return Math.min(sumP, 1);
-    }
+		// The algorithm used requires the following assumptions to be true:
+		// - the top left to bottom right diagonal contains the smaller values
+		// - within both diagonals, the top row contains the smaller of the two values
+		if (a * d > b * c) {
+			tmp = a;
+			a = b;
+			b = tmp;
 
-    public static void main(String[] args) {
+			tmp = c;
+			c = d;
+			d = tmp;
+		}
+		if (a > d) {
+			tmp = a;
+			a = d;
+			d = tmp;
+		}
+		if (b > c) {
+			tmp = b;
+			b = c;
+			c = tmp;
+		}
+
+		int original_a = a;
+		double sumP = 0;
+
+		// Here we iteratively compute the one-tailed p-value: the sum of all intermediate p-values
+		// while iteratively shifting counts from the a/d diagonal to b/c.
+		double p = computePval(a, b, c, d);
+		double p_1 = p;
+
+		while (a >= 0) {
+			sumP += p;
+			if (a == 0) {
+				break;
+			}
+			a--;
+			b++;
+			c++;
+			d--;
+			p = computePval(a, b, c, d);
+		}
+		if (oneTailed) {
+			return Math.min(sumP, 1);
+		}
+
+		// For the two-tailed statistic we swap the table diagonals and repeat the iterative process
+		a = b;
+		b = 0;
+		c = c - a;
+		d = d + a;
+		p = computePval(a, b, c, d);
+
+		while (p < p_1) {
+			if (a == original_a) {
+				break;
+			}
+			sumP += p;
+			a--;
+			b++;
+			c++;
+			d--;
+			p = computePval(a, b, c, d);
+		}
+		return Math.min(sumP, 1);
+	}
+
+	/**
+	 * @return A single iteration of the p-value computation for the given counts
+	 */
+	private static double computePval(int a, int b, int c, int d) {
+		return Math.exp(
+										(CACHED_LOG_FACTORIAL.get(a + b) +
+										 CACHED_LOG_FACTORIAL.get(c + d) +
+										 CACHED_LOG_FACTORIAL.get(a + c) +
+										 CACHED_LOG_FACTORIAL.get(b + d))
+										-
+										(CACHED_LOG_FACTORIAL.get(a + b + c + d) +
+										 CACHED_LOG_FACTORIAL.get(a) +
+										 CACHED_LOG_FACTORIAL.get(b) +
+										 CACHED_LOG_FACTORIAL.get(c) +
+										 CACHED_LOG_FACTORIAL.get(d)));
+	}
+
+	/**
+	 * Check if we have enough cached log factorial values for the given table counts. If not, expand
+	 * the table cache.
+	 */
+	private static void ensureCacheSize(int n1, int n2, int n3, int n4) {
+		int minSize = 1 + n1 + n2 + n3 + n4;
+		if (CACHED_LOG_FACTORIAL.size() < minSize) {
+			buildCachedEntries(minSize);
+		}
+	}
+
+	/**
+	 * @param minSize Number of log factorial entries to ensure are cached in our table
+	 */
+	private static void buildCachedEntries(int minSize) {
+		synchronized (CACHED_LOG_FACTORIAL) {
+			if (CACHED_LOG_FACTORIAL.isEmpty()) {
+				// Seed the initial entry
+				CACHED_LOG_FACTORIAL.add(0.0);
+			}
+			for (int i = CACHED_LOG_FACTORIAL.size(); i <= minSize; i++) {
+				CACHED_LOG_FACTORIAL.add(CACHED_LOG_FACTORIAL.get(i - 1) + Math.log(i));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
 		double chiP, fishP;
 		int[][][] testMatrices = new int[][][] {
-				{{0, 20}, {0, 25}},
-				{{2, 20}, {3, 25}},
-				{{0, 20}, {5, 20}},
-				{{0, 2100}, {0, 2500}},
-				{{0, 2100}, {5, 2500}},
-				{{0, 2100}, {10, 2500}},
-				{{210, 2100}, {250, 2500}},
+																						{{0, 20}, {0, 25}},
+																						{{2, 20}, {3, 25}},
+																						{{0, 20}, {5, 20}},
+																						{{0, 2100}, {0, 2500}},
+																						{{0, 2100}, {5, 2500}},
+																						{{0, 2100}, {10, 2500}},
+																						{{210, 2100}, {250, 2500}},
 		};
-		
-		FishersExact2by2Calculator calc = new FishersExact2by2Calculator();
-		
+
 		System.out.println("chi^2 p\tFisher's Exact p");
 		for (int i = 0; i < testMatrices.length; i++) {
 			chiP = ProbDist.ChiDist(ContingencyTable.ChiSquare(testMatrices[i], false), 1);
-			calc.loadMatrix(testMatrices[i]);
-			fishP = calc.getPvalue();
-			System.out.println(ext.prettyP(chiP)+"\t"+fishP);
-		}			
+			fishP = FishersExact2by2Calculator.getPvalue(testMatrices[i]);
+			System.out.println(ext.prettyP(chiP) + "\t" + fishP);
+		}
 	}
 }
---
+++ Genvisis/src/test/java/org/genvisis/stats/TestFishersExact.java
@@ -0,0 +1,27 @@
+package org.genvisis.stats;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link FishersExact2by2Calculator}
+ */
+public class TestFishersExact {
+
+	/**
+	 * Test basic expected values from <a href="https://en.wikipedia.org/wiki/Fisher%27s_exact_test">the wikipedia article</a>.
+	 */
+	@Test
+	public void testExpected() {
+		double expected = 0.000033652;
+
+		// Test array input
+		double pVal = FishersExact2by2Calculator.getPvalue(new int[][] {{0, 10}, {12, 2}});
+		assertEquals(expected, pVal, 0.000000001);
+
+		// Should be the same for individual values
+		pVal = FishersExact2by2Calculator.getPvalue(0, 10, 12, 2);
+		assertEquals(expected, pVal, 0.000000001);
+	}
+}
