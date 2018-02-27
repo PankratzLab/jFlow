325d9f606b0cbd591668c4b66873f053d60f2b2b
Spencer Kelley <kelle881@umn.edu>
2018-02-27 10:48:03 -0600

    Introduce ColumnFilters static utility class

---
+++ Genvisis/src/main/java/org/genvisis/gwas/parsing/ColumnFilters.java
@@ -0,0 +1,21 @@
+package org.genvisis.gwas.parsing;
+
+public class ColumnFilters {
+
+  private ColumnFilters() {}
+
+  public static OrFilter or(ColumnFilter... filters) {
+    return new OrFilter(filters);
+  }
+
+  public static ColumnFilter not(ColumnFilter filter) {
+    return new AbstractColumnFilter(filter.getFilterColumns()) {
+
+      @Override
+      public boolean filter(DataLine values) {
+        return !filter.filter(values);
+      }
+    };
+  }
+
+}
