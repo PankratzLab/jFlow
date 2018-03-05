ff0970eb9621adf96344d4b6fe57d28366372dcc
Spencer Kelley <kelle881@umn.edu>
2018-03-05 12:09:11 -0600

    Introduce ArrayWrapperColumn

---
+++ Genvisis/src/main/java/org/genvisis/gwas/parsing/ArrayWrapperColumn.java
@@ -0,0 +1,30 @@
+package org.genvisis.gwas.parsing;
+
+public class ArrayWrapperColumn extends CachedFileColumn<String[]> {
+
+  private final FileColumn<String> base;
+  private final String delimeter;
+
+  /**
+   * Construct an {@link ArrayWrapperColumn} that parses into a String[] based on delimiter
+   * 
+   * @param base underlying column to split into array
+   * @param delimeter regex delimiter to split on
+   */
+  public ArrayWrapperColumn(FileColumn<String> base, String delimeter) {
+    super(base.getName(), base.dieOnParseFailure());
+    this.base = base;
+    this.delimeter = delimeter;
+  }
+
+  @Override
+  public String[] calculateValue(String[] line) throws ParseFailureException {
+    return base.getValue(line).split(delimeter);
+  }
+
+  @Override
+  public void initialize(FileParser parser) {
+    base.initialize(parser);
+  }
+
+}
