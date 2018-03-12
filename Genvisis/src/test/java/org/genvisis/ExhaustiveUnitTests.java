3bdf5df661961863767c2371eea1c30b52504e9d
Spencer Kelley <kelle881@umn.edu>
2018-03-12 13:38:43 -0500

    Add ExhaustiveUnitTests and set up fastTest profile to not include them
    Run with maven property fastTests=true

---
+++ Genvisis/src/test/java/org/genvisis/ExhaustiveUnitTests.java
@@ -0,0 +1,7 @@
+package org.genvisis;
+
+/**
+ * Marker for Exhuastive Unit Tests, not to be run for every personal build to alleviate long build
+ * times
+ */
+public interface ExhaustiveUnitTests {}
