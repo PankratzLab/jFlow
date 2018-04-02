5bef3e75f88ff7b43ba26e61449de5aafa97ec52
MR <mary.rachel.stimson@gmail.com>
2018-04-02 11:06:40 -0500

    Class to match file columns based on a regex

---
+++ Genvisis/src/main/java/org/genvisis/common/parsing/PatternedFileColumn.java
@@ -0,0 +1,70 @@
+package org.genvisis.common.parsing;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class PatternedFileColumn extends AbstractFileColumn<String> implements IndexedFileColumn<String> {
+
+  private final String regex;
+  private int matchedIndex;
+  private String matchedHeader = null;
+
+  public PatternedFileColumn(String nm, String regex) {
+    super(nm);
+    this.regex = regex;
+  }
+
+  @Override
+  public void initialize(FileParser parser) {
+    super.initialize(parser);
+    if (matchedHeader != null) return;
+    Map<String, Integer> headerMap = parser.getHeaderMap();
+    if (headerMap == null) {
+      throw new IllegalStateException("Column " + getName() + " expected a header for file "
+                                      + parser.getInputFile());
+    }
+
+    int matched = -1;
+    String colname = "";
+
+    for (String key : headerMap.keySet()) {
+      if (Pattern.matches(regex, key)) {
+        if (matched > -1) {
+          // pick the earliest index we encounter
+          colname = headerMap.get(key) < matched ? key : colname;
+          matched = Math.min(matched, headerMap.get(key));
+        } else {
+          matched = headerMap.get(key);
+          colname = key;
+        }
+      }
+    }
+
+    if (matched == -1) {
+      throw new IllegalStateException("Couldn't identify column header for " + getName()
+                                      + " in file " + parser.getInputFile());
+    }
+
+    this.matchedHeader = colname;
+    this.matchedIndex = matched;
+  }
+
+  @Override
+  public String getValue(String[] line) throws ParseFailureException {
+    return line[matchedIndex];
+  }
+
+  /**
+   * @return Index of column in data based on matched header
+   */
+  @Override
+  public int getIndex() {
+    return matchedIndex;
+  }
+
+  @Override
+  public String getHeader() {
+    return matchedHeader;
+  }
+
+}
