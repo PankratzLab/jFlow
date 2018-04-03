2a4ace3853fc4526e7c13c6f6f9b3b0409f27c9a
jlanej <jlanej@gmail.com>
2018-04-03 15:11:56 -0500

    CramStats: one-off to pull out sample info from .cram header
    *SEQUENCING_CENTER
    *DATE_RUN_PRODUCED
    *PLATFORM
    *LIBRARY
    
    etc

---
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/CramStats.java
@@ -0,0 +1,73 @@
+package org.genvisis.one.JL.topMed;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+import java.util.StringJoiner;
+import org.genvisis.common.Files;
+import org.genvisis.common.Logger;
+import org.genvisis.seq.manage.BamOps;
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMReadGroupRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.ValidationStringency;
+
+public class CramStats {
+
+  public static void main(String[] args) {
+    String dir = args[1];
+    String ref = args[2];
+    String out = args[3];
+
+    System.setProperty("samjdk.reference_fasta", ref);
+
+    new File(out).mkdirs();
+    String[] crams = Files.listFullPaths(dir, ".cram");
+    System.out.println("found " + crams.length + " crams");
+
+    String outFile = out + "summary.txt";
+    PrintWriter writer = Files.getAppropriateWriter(outFile);
+
+    writer.println("Sample\tSEQUENCING_CENTER\tDATE_RUN_PRODUCED\tPLATFORM\tLIBRARY\tDESCRIPTION\tREAD_LENGTH");
+
+    for (String cram : crams) {
+
+      SamReaderFactory samReaderFactory = SamReaderFactory.makeDefault();
+      samReaderFactory.validationStringency(ValidationStringency.STRICT);
+
+      SamReader reader = samReaderFactory.open(new File(cram));
+
+      SAMFileHeader header = reader.getFileHeader();
+      int rl = BamOps.estimateReadSize(cram, new Logger());
+      List<SAMReadGroupRecord> rgs = header.getReadGroups();
+      for (SAMReadGroupRecord rg : rgs) {
+
+        //        System.out.println(rg.getLibrary());
+        System.out.println(rg);
+
+        StringJoiner joiner = new StringJoiner("\t");
+        joiner.add(rg.getSample());
+        joiner.add(rg.getSequencingCenter());
+        joiner.add(rg.getRunDate() == null ? new Date().toString() : rg.getRunDate().toString());
+        joiner.add(rg.getPlatform());
+        joiner.add(rg.getLibrary());
+        joiner.add(rg.getDescription());
+        joiner.add(Double.toString(rl));
+
+        writer.println(joiner.toString());
+
+      }
+
+      try {
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    writer.close();
+
+  }
+}
