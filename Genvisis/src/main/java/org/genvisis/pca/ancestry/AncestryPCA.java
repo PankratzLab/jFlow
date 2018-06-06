e1860c02ddbb0b18b6eeb8a4dc8cafc89fb53b6d
jlanej <jlanej@gmail.com>
2018-06-06 18:11:28 -0500

    Comments

--- Genvisis/src/main/java/org/genvisis/pca/ancestry/AncestryPCA.java
+++ Genvisis/src/main/java/org/genvisis/pca/ancestry/AncestryPCA.java
@@ -11,7 +11,7 @@ import com.google.common.math.Stats;
 import com.google.common.math.StatsAccumulator;
 
 /**
- * Runs PCA starting from a {@link NamedRealMatrix}
+ * Runs PCA that focuses on ancestry PCS (prepares genotype data for ancestry PCA)
  */
 public class AncestryPCA {
 
@@ -19,21 +19,27 @@ public class AncestryPCA {
 
   }
 
+  /**
+   * @param loader {@link MatrixDataLoading} that provides genotypes in double format (0,1,2,NaN)
+   * @param log
+   * @return {@link SVD} holding results
+   */
   public static SVD generatePCs(MatrixDataLoading loader, Logger log) {
     NamedRealMatrix m = loader.getData();
 
-    prepareForPCA(m, log);
+    normalizeGenotypeData(m, log);
     return computePCA(m, log);
   }
 
   /**
-   * Prepares genotype data for PCA (see https://www.nature.com/articles/ng1847)
+   * Prepares genotype data for PCA (see https://www.nature.com/articles/ng1847). Can also be used
+   * to prepare data for extrapolating PCs
    * 
-   * @param m
+   * @param m {@link NamedRealMatrix} with genotypes to prepare
    * @param log
    * @return
    */
-  public static void prepareForPCA(NamedRealMatrix m, Logger log) {
+  static void normalizeGenotypeData(NamedRealMatrix m, Logger log) {
     //    Let gij be a matrix of genotypes for SNP i and individual j, where i = 1 to M 
     //        and j = 1 to N. We subtract the row mean μi = (Σjgij)/N from each entry in row i 
     //        to obtain a matrix with row sums equal to 0; missing entries are excluded from
@@ -47,7 +53,7 @@ public class AncestryPCA {
       for (int column = 0; column < m.getM().getColumnDimension(); column++) {
         double val = m.getM().getEntry(row, column);
         if (valid(val)) {
-          if (Double.isFinite(val)) {
+          if (isNonMissing(val)) {
             statsAccumulator.add(val);
           }
         } else {
@@ -57,20 +63,24 @@ public class AncestryPCA {
         }
       }
       Stats stats = statsAccumulator.snapshot();
-      double pi = (1 + stats.sum()) / ((double) 2 + 2 * m.getM().getColumnDimension());
+      double possible = (double) (2 + 2 * m.getM().getColumnDimension());
+      double pi = (1 + stats.sum()) / possible;
       double norm = Math.sqrt(pi * (1 - pi));
       for (int column = 0; column < m.getM().getColumnDimension(); column++) {
         double val = m.getM().getEntry(row, column);
-        if (Double.isFinite(val)) {
+        if (isNonMissing(val)) {
           double mc = val - stats.mean();
           mc /= norm;
           m.getM().setEntry(row, column, mc);
         } else {
           m.getM().setEntry(row, column, 0);
-
         }
       }
     }
+  }
+
+  private static boolean isNonMissing(double val) {
+    return Double.isFinite(val) && val >= 0;
   }
 
   private static boolean valid(double val) {
@@ -78,6 +88,12 @@ public class AncestryPCA {
     return Double.isNaN(val) || val == 2 || val == 1 || val == 0;
   }
 
+  /**
+   * @param m {@link NamedRealMatrix} that has been prepared with
+   *          {@link AncestryPCA#normalizeGenotypeData(NamedRealMatrix, Logger)}
+   * @param log
+   * @return {@link SVD}
+   */
   private static SVD computePCA(NamedRealMatrix m, Logger log) {
 
     return SVD.computeSVD(m, log);
