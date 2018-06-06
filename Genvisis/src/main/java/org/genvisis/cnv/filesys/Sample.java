e99b5a13d02c961337f5185a69c27116088b0d6b
Spencer Kelley <kelle881@umn.edu>
2018-06-06 15:47:44 -0500

    Add centroid calculated versions of marker LRR and BAF maps

--- Genvisis/src/main/java/org/genvisis/cnv/filesys/Sample.java
+++ Genvisis/src/main/java/org/genvisis/cnv/filesys/Sample.java
@@ -372,12 +372,16 @@ public class Sample implements Serializable {
    * @return Map from {@link MarkerDetailSet.Marker} to BAF for this {@link Sample}
    */
   public Map<Marker, Double> markerBAFMap(MarkerDetailSet markerSet) {
-    List<Marker> projOrderMarkers = markerSet.markersAsList();
-    ImmutableMap.Builder<Marker, Double> markerBAFMapBuilder = ImmutableMap.builder();
-    for (int i = 0; i < bafs.length; i++) {
-      markerBAFMapBuilder.put(projOrderMarkers.get(i), (double) bafs[i]);
-    }
-    return markerBAFMapBuilder.build();
+    return floatsToDoubleMap(getBAFs(), markerSet);
+  }
+
+  /**
+   * @param markerSet
+   * @param centroids
+   * @return Map from {@link MarkerDetailSet.Marker} to BAF for this {@link Sample}
+   */
+  public Map<Marker, Double> markerBAFMap(MarkerDetailSet markerSet, float[][][] centroids) {
+    return floatsToDoubleMap(getBAFs(centroids), markerSet);
   }
 
   public float[] getBAFs() {
@@ -408,16 +412,20 @@ public class Sample implements Serializable {
    * @return Map from {@link MarkerDetailSet.Marker} to LRR for this {@link Sample}
    */
   public Map<Marker, Double> markerLRRMap(MarkerDetailSet markerSet) {
-    List<Marker> projOrderMarkers = markerSet.markersAsList();
-    ImmutableMap.Builder<Marker, Double> markerLRRMapBuilder = ImmutableMap.builder();
-    for (int i = 0; i < lrrs.length; i++) {
-      markerLRRMapBuilder.put(projOrderMarkers.get(i), (double) lrrs[i]);
-    }
-    return markerLRRMapBuilder.build();
+    return floatsToDoubleMap(getLRRs(), markerSet);
   }
 
   public float[] getLRRs() {
     return lrrs;
+  }
+
+  /**
+   * @param markerSet
+   * @param centroids
+   * @return Map from {@link MarkerDetailSet.Marker} to LRR for this {@link Sample}
+   */
+  public Map<Marker, Double> markerLRRMap(MarkerDetailSet markerSet, float[][][] centroids) {
+    return floatsToDoubleMap(getLRRs(centroids), markerSet);
   }
 
   public float[] getLRRs(float[][][] centroids) {
@@ -438,6 +446,15 @@ public class Sample implements Serializable {
     }
 
     return lrrs;
+  }
+
+  public static Map<Marker, Double> floatsToDoubleMap(float[] lrrs, MarkerDetailSet markerSet) {
+    List<Marker> projOrderMarkers = markerSet.markersAsList();
+    ImmutableMap.Builder<Marker, Double> markerLRRMapBuilder = ImmutableMap.builder();
+    for (int i = 0; i < lrrs.length; i++) {
+      markerLRRMapBuilder.put(projOrderMarkers.get(i), (double) lrrs[i]);
+    }
+    return markerLRRMapBuilder.build();
   }
 
   /**
