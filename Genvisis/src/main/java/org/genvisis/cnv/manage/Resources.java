3891873fe289a9e3bdb8c0c394e94bdfa0e0f3c3
Spencer Kelley <kelle881@umn.edu>
2018-08-07 14:36:43 -0500

    Define PAR boundaries in GENOME_BUILD

--- Genvisis/src/main/java/org/genvisis/cnv/manage/Resources.java
+++ Genvisis/src/main/java/org/genvisis/cnv/manage/Resources.java
@@ -1402,14 +1402,21 @@ public final class Resources {
    * Supported genome reference builds
    */
   public enum GENOME_BUILD {
-    HG38("hg38", 38), HG19("hg19", 37), HG18("hg18", 36);
+    HG38("hg38", 38, 2781479, 155701383),
+    HG19("hg19", 37, 2699520, 154931044),
+    HG18("hg18", 36, 2709521, 154584237);
 
     private final String build;
     private final int buildInt;
+    // Matched to the PAR definitions from plink: https://www.cog-genomics.org/plink/1.9/data#split_x
+    private final int par1End;
+    private final int par2Start;
 
-    private GENOME_BUILD(String build, int buildInt) {
+    private GENOME_BUILD(String build, int buildInt, int par1_end, int par2_start) {
       this.build = build;
       this.buildInt = buildInt;
+      this.par1End = par1_end;
+      this.par2Start = par2_start;
     }
 
     public String getBuild() {
@@ -1419,6 +1426,21 @@ public final class Resources {
     public int getBuildInt() {
       return buildInt;
     }
+
+    /**
+     * @return the last position of x chromosome PAR 1
+     */
+    public int getPAR1End() {
+      return par1End;
+    }
+
+    /**
+     * @return the first position of x chromosome PAR 2
+     */
+    public int getPAR2Start() {
+      return par2Start;
+    }
+
   }
 
   /**
