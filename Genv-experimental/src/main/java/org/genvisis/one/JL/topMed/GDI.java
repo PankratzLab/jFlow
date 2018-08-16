783d118cd63926c975451016d1c0e9d85c8da952
jlanej <jlanej@gmail.com>
2018-08-16 14:45:27 -0500

    GDI: trim a vcf to variants that could be used (by function)

--- Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.util.List;
 import org.genvisis.CLI;
 import org.genvisis.common.Logger;
+import org.genvisis.common.ext;
 import org.genvisis.one.JL.topMed.TOPMedUtils.GeneImpact;
+import org.genvisis.one.JL.topMed.TOPMedUtils.IMPACT;
+import org.genvisis.seq.manage.VCFOps;
 import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.writer.Options;
+import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+import htsjdk.variant.variantcontext.writer.VariantContextWriterBuilder;
 import htsjdk.variant.vcf.VCFFileReader;
 
 /**
@@ -14,19 +20,38 @@ import htsjdk.variant.vcf.VCFFileReader;
  */
 public class GDI {
 
-  private static void run(CLI c) {
+  private static final String TRIM_TO_GENES = "trimToGenes";
+
+  private static void trimToImpactVariants(CLI c) {
     String outDir = c.get(CLI.ARG_OUTDIR);
     new File(outDir).mkdirs();
     Logger log = new Logger(outDir + "gdi.log");
     String vcf = c.get(CLI.ARG_VCF);
 
-    log.reportTimeInfo("computing GDI from " + vcf);
+    log.reportTimeInfo("selecting variants from " + vcf);
     VCFFileReader reader = new VCFFileReader(new File(vcf));
+
+    String outputVcf = outDir + VCFOps.getAppropriateRoot(vcf, true) + ".func.vcf.gz";
+    VariantContextWriterBuilder builder = new VariantContextWriterBuilder().setOutputFile(outputVcf);
+    builder.setReferenceDictionary(reader.getFileHeader().getSequenceDictionary());
+
+    VariantContextWriter writer = builder.build();
+    writer.writeHeader(reader.getFileHeader());
 
     CloseableIterator<VariantContext> iter = reader.iterator();
     while (iter.hasNext()) {
       VariantContext vc = iter.next();
       List<GeneImpact> geneImpacts = TOPMedUtils.getAllGeneImpacts(vc);
+      boolean use = false;
+      for (GeneImpact g : geneImpacts) {
+        if (g.impact.ordinal() > IMPACT.LOW.ordinal()) {
+          use = true;
+          break;
+        }
+      }
+      if (use) {
+        writer.add(vc);
+      }
     }
     reader.close();
 
@@ -35,14 +60,15 @@ public class GDI {
   public static void main(String[] args) {
     CLI c = new CLI(GDI.class);
 
+    c.addFlag(TRIM_TO_GENES, "trim the .vcf to variants with a gene annotation");
     c.addArg(CLI.ARG_VCF, CLI.DESC_VCF);
-    //    c.addArg("bed","bed file containing ");
-
     c.addArg(CLI.ARG_OUTDIR, CLI.DESC_OUTDIR);
 
     c.parseWithExit(args);
 
-    run(c);
+    if (c.has(TRIM_TO_GENES)) {
+      trimToImpactVariants(c);
+    }
   }
 
 }
