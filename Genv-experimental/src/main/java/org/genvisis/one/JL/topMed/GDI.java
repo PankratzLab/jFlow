4d24a230590e6b53a11e27792c3dd4973b36190a
jlanej <jlanej@gmail.com>
2018-08-16 15:18:26 -0500

    GDI: add cookbook

--- Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
@@ -4,16 +4,24 @@ import java.io.File;
 import java.util.List;
 import org.genvisis.CLI;
 import org.genvisis.common.Logger;
-import org.genvisis.common.ext;
 import org.genvisis.one.JL.topMed.TOPMedUtils.GeneImpact;
 import org.genvisis.one.JL.topMed.TOPMedUtils.IMPACT;
 import org.genvisis.seq.manage.VCFOps;
 import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.variantcontext.writer.Options;
 import htsjdk.variant.variantcontext.writer.VariantContextWriter;
 import htsjdk.variant.variantcontext.writer.VariantContextWriterBuilder;
 import htsjdk.variant.vcf.VCFFileReader;
+
+// : (i) “raw” GDI, calculated for each human gene by first multiplying each variant’s CADD score by
+// the corresponding variant’s number of alleles in the 1,000 Genomes Project (a total of 610,160
+// missense/nonsense/frameshift/in-frame indels/splice variants, with a MAF < 0.5, from a total of
+// 20,243,313 alleles), then summing up all (CADD × allele count) products for one gene; (ii) the
+// “CADD-normalized” gene-level model of accumulated mutational damage, calculated as in i, with
+// each CADD score divided by the expected (median) CADD score of a variant with a similar allele
+// frequency (Fig. S2A);
+// http://www.pnas.org/content/112/44/13615.long
+// http://www.pnas.org/content/112/44/13615.long#F2
 
 /**
  * Prototype to compute GDI using TOPMed
