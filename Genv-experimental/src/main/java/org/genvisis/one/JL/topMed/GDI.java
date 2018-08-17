69d4c4cc9f0437f88f75ab8c1914802a5fb583dd
jlanej <jlanej@gmail.com>
2018-08-17 17:11:31 -0500

    GDI: more language from paper

--- Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/GDI.java
@@ -25,6 +25,27 @@ import htsjdk.variant.vcf.VCFFileReader;
 // “CADD-normalized” gene-level model of accumulated mutational damage, calculated as in i, with
 // each CADD score divided by the expected (median) CADD score of a variant with a similar allele
 // frequency (Fig. S2A)
+
+// For each human gene g with n minor alleles (missense/nonsense/frameshift/in-frame indels/splice,
+// with MAF < 0.5), we calculated GDIg, the cumulative predicted damage to the exonic regions of the
+// gene, by multiplying the CADD raw C score by the count f of each allele A and dividing the
+// expected CE score for a variant with a corresponding allele frequency (calculated by the 1,000
+// Genomes Project Phase 3 median C score for each allele frequency slot; Fig. S2A) and then summing
+// the results: GDIg=∑nA=1(CACE)fA
+// . We calculated a homogenized Phred I-score for each metric, indicating the ranking of the gene
+// of interest i relative to all other human genes (T = 19,558 genes in our analyses), with values
+// ranging from 0 (lowest Phred score: human gene with lowest GDI) to 42.91 (highest Phred score:
+// most damaged human gene): Ii=−10[log10(i/T)]
+// . The GDI model described had the highest performance, in three alternative forms: (i) raw GDI,
+// as above but without expected CADD score normalization: GDIg=∑nA=1CAfA
+// ; (ii) observed/expected GDI-normalized model, with raw GDI calculated by dividing the observed
+// CADD score values CO by the expected CADD score CE, determined by calculating the 1,000 Genomes
+// Project Phase 3 median C score for each allele frequency slot: GDIg=(∑nA=1COfA)/(∑nA=1CEfA)
+// ; and (iii) gene size-normalized GDI, where the raw GDI score was normalized by dividing by the
+// CDS length L of the gene’s canonical transcript: GDIg=(∑nA=1COfA)/Lg
+// .
+//
+
 // http://www.pnas.org/content/112/44/13615.long
 // http://www.pnas.org/content/112/44/13615.long#F2
 
@@ -104,45 +125,45 @@ public class GDI {
 
     log.reportTimeInfo("computing GDI using variants from " + vcf);
 
-    VCFFileReader reader = new VCFFileReader(new File(vcf));
-    String outputTmp = outDir + VCFOps.getAppropriateRoot(vcf, true) + ".cadd.af.txt.gz";
-    CloseableIterator<VariantContext> iter = reader.iterator();
-    int numTotal = 0;
-    int numUsed = 0;
-    int numSkipped = 0;
+    try (VCFFileReader reader = new VCFFileReader(new File(vcf))) {
+      String outputTmp = outDir + VCFOps.getAppropriateRoot(vcf, true) + ".cadd.af.txt.gz";
+      CloseableIterator<VariantContext> iter = reader.iterator();
+      int numTotal = 0;
+      int numUsed = 0;
+      int numSkipped = 0;
 
-    PrintWriter writer = Files.getAppropriateWriter(outputTmp);
-    writer.println(ArrayUtils.toStr(BASE) + "\t" + ArrayUtils.toStr(ANNOS));
+      PrintWriter writer = Files.getAppropriateWriter(outputTmp);
+      writer.println(ArrayUtils.toStr(BASE) + "\t" + ArrayUtils.toStr(ANNOS));
 
-    while (iter.hasNext()) {
-      numTotal++;
-      VariantContext vc = iter.next();
-      if (!vc.isFiltered()) {
+      while (iter.hasNext()) {
+        numTotal++;
+        VariantContext vc = iter.next();
+        if (!vc.isFiltered()) {
 
-        try {
-          Double.parseDouble(vc.getAttributeAsString("CADD_raw", "."));
-          StringJoiner out = new StringJoiner("\t");
-          out.add(vc.getContig());
-          out.add(Integer.toString(vc.getStart()));
-          out.add(Integer.toString(vc.getEnd()));
-          out.add(vc.getID());
-          out.add(vc.getReference().getBaseString());
-          out.add(vc.getAlternateAlleles().get(0).getBaseString());
+          try {
+            Double.parseDouble(vc.getAttributeAsString("CADD_raw", "."));
+            StringJoiner out = new StringJoiner("\t");
+            out.add(vc.getContig());
+            out.add(Integer.toString(vc.getStart()));
+            out.add(Integer.toString(vc.getEnd()));
+            out.add(vc.getID());
+            out.add(vc.getReference().getBaseString());
+            out.add(vc.getAlternateAlleles().get(0).getBaseString());
 
-          writer.println(out.toString() + "\t"
-                         + ArrayUtils.toStr(VCOps.getAnnotationsFor(ANNOS, vc, ".")));
-          numUsed++;
-        } catch (NumberFormatException nfe) {
-          numSkipped++;
-        }
+            writer.println(out.toString() + "\t"
+                           + ArrayUtils.toStr(VCOps.getAnnotationsFor(ANNOS, vc, ".")));
+            numUsed++;
+          } catch (NumberFormatException nfe) {
+            numSkipped++;
+          }
 
-        if (numTotal % 10000 == 0) {
-          log.reportTimeInfo("processed " + numTotal + " variants, retained " + numUsed
-                             + " skipped " + numSkipped + " for missing CADD");
+          if (numTotal % 10000 == 0) {
+            log.reportTimeInfo("processed " + numTotal + " variants, retained " + numUsed
+                               + " skipped " + numSkipped + " for missing CADD");
+          }
         }
       }
     }
-    reader.close();
   }
 
   public static void main(String[] args) {
