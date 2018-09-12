81bf872d8f44616b456951a69dd3094457c346c9
jlanej <jlanej@gmail.com>
2018-09-12 11:39:18 -0500

    AddWGSA: ensure annotated variant is present in study
    Also only uses unique variants

--- Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/AddWGSA.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/AddWGSA.java
@@ -27,6 +27,16 @@ import htsjdk.variant.vcf.VCFInfoHeaderLine;
 
 public class AddWGSA {
 
+  //  Important notice:
+  //    1. To facilitate gene-based genotype-phenotype association analysis, the most "deleterious" consequence of the variant for its corresponding gene or genes were identified according to
+  //            http://www.ensembl.org/info/genome/variation/predicted_data.html#consequences. Each vairant may contain multiple rows in freeze.6.chr1-22X.snp.general.gz
+  //            and freeze.6.chr1-22X.indel.general.gz, one row for a gene. To obtain only one row for a variant, I recommend to use the row with "Y" in the unique_variant column,
+  //            which corresponds to the most "deleterious" consequence of the variant across genes it impacts.
+  //    2. The coordinates of hg38 were converted to those of hg19 via CrossMap (https://doi.org/10.1093/bioinformatics/btt730). There are a few cases in which the reference allele of the hg19 coordinate does not agree with
+  //            that of the hg38 coordinate. Those variants will have a "N" in column ref_hg19_equals_ref_hg38 and a "." in column alt_hg19.
+  //    3. When an annotation resource has both hg38 and hg19 data, the hg38 will be used in priority. When an annotation resource only has hg19 data, the annotation will be based on the CrossMap converted hg19 coordiate
+  //            (and ref_hg19 and alt_hg19 if applicable).
+  //    4. Because there are hundreds of annotation columns for each variant, I recommend read "List of resources v0.75.docx" first before resorting the column description files (description.txt)
   private static final String WGSA_SNP_FILE = "wgsaSnpFile";
   private static final String WGSA_INDEL_FILE = "wgsaIndelFile";
 
@@ -35,6 +45,8 @@ public class AddWGSA {
   private static final String REF = "ref";
   private static final String ALT = "alt";
   private static final String DB_SNP = "rs_dbSNP150";
+  private static final String TOPMED_FRZ6_AC = "TOPMed_frz6_AC";
+  private static final String UNIQUE_VARIANT = "unique_variant";
 
   private static final List<String> VARIANT_KEYS = Arrays.asList(CHR, POS, REF, ALT);
 
@@ -78,85 +90,159 @@ public class AddWGSA {
           unique.addAll(interestSNP);
           unique.addAll(interestIndel);
 
-          addHeader(unique, reader.getFileHeader(), writer);
-
+          addHeader(unique, reader.getFileHeader(), writer, log);
+          int num = 0;
+          int numSNPsTransferred = 0;
+          int numIndelsTransferred = 0;
+          WGSALine wSNPLine = processLine(lookupSNP, readerAnnotationSNP, interestSNP, log);
+          WGSALine wIndelLine = processLine(lookupIndel, readerAnnotationIndel, interestIndel, log);
           for (VariantContext vc : reader) {
+            num++;
+            if (num % 1 == 0) {
+              log.reportTimeInfo("Processed " + num + " variants\nNum SNP annotations transferred="
+                                 + numSNPsTransferred + "\nNum INDEL annotations transferred="
+                                 + numIndelsTransferred);
 
+              //              if (vc.getStart() == 5031125) {
+              //
+              log.reportTimeInfo(vc.toStringWithoutGenotypes() + "\t" + wIndelLine.line[1] + "\t"
+                                 + wIndelLine.line[2] + "\t" + wIndelLine.line[3] + "\t"
+                                 + vc.getReference().getBaseString() + "\t"
+                                 + vc.getAlternateAllele(0).getBaseString() + "\t"
+                                 + vc.getReference().getBaseString().equals(wIndelLine.line[2])
+                                 + vc.getAlternateAllele(0).getBaseString()
+                                     .equals(wIndelLine.line[3]));
+              log.reportTimeInfo(vc.toStringWithoutGenotypes() + "\t" + wSNPLine.line[1]);
+            }
+            //          }
+            //            WANT
+            //            CTTTTTT
+            //            CTTTTTTT
+            //            CTTTTTTT
+            //            
+            //            
+            //            
+            //            CTTTTTTT
+            //            CTTTTTT
+            //            CTTTTTT
+            //            CTTTTTTT
+            //            CTTTTTTT
             VariantContextBuilder vcBuilder = new VariantContextBuilder(vc);
-            Map<String, String> toAdd = new HashMap<>();
-
-            if (vc.isIndel()) {
-              processLine(lookupIndel, readerAnnotationIndel, interestIndel, vc, toAdd);
-
-            } else {
-              processLine(lookupSNP, readerAnnotationSNP, interestSNP, vc, toAdd);
-            }
-            for (Entry<String, String> entry : toAdd.entrySet()) {
-              vcBuilder.attribute(entry.getKey(), entry.getValue());
-            }
-            if (".".equals(vc.getID())) {
-              vcBuilder.id(toAdd.get(DB_SNP));
+            if (validate(lookupIndel, vc, wIndelLine.line)) {
+              numSNPsTransferred++;
+              transferAnnotations(vc, vcBuilder, wIndelLine.toAdd);
+              wIndelLine = processLine(lookupIndel, readerAnnotationIndel, interestIndel, log);
+            } else if (validate(lookupSNP, vc, wSNPLine.line)) {
+              transferAnnotations(vc, vcBuilder, wSNPLine.toAdd);
+              numIndelsTransferred++;
+              wSNPLine = processLine(lookupSNP, readerAnnotationSNP, interestSNP, log);
             }
             writer.add(vcBuilder.make());
           }
           writer.close();
         }
       }
-    } catch (IOException e) {
+    } catch (
+
+    IOException e) {
       log.reportException(e);
 
     }
   }
 
-  private static Map<String, Integer> processHeader(BufferedReader readerAnnotationIndel) throws IOException {
+  static void transferAnnotations(VariantContext vc, VariantContextBuilder vcBuilder,
+                                  Map<String, String> toAdd) {
+    for (Entry<String, String> entry : toAdd.entrySet()) {
+      vcBuilder.attribute(entry.getKey(), entry.getValue());
+    }
+    if (".".equals(vc.getID())) {
+      vcBuilder.id(toAdd.get(DB_SNP));
+    }
+  }
+
+  private static Map<String, Integer> processHeader(BufferedReader readerAnnotation) throws IOException {
     Map<String, Integer> lookup = new HashMap<>();
-    String[] headerIndel = readerAnnotationIndel.readLine().trim().split("\t");
-    for (int i = 0; i < headerIndel.length; i++) {
-      lookup.put(headerIndel[i], i);
+    String[] header = readerAnnotation.readLine().trim().split("\t");
+    for (int i = 0; i < header.length; i++) {
+      header[i] = header[i].replaceAll("=", ".eq.");
+      lookup.put(header[i], i);
     }
     return lookup;
   }
 
-  private static void processLine(Map<String, Integer> lookup, BufferedReader reader,
-                                  Set<String> interest, VariantContext vc,
-                                  Map<String, String> toAdd) throws IOException {
-    String[] line = reader.readLine().trim().split("\t");
-    validate(lookup, vc, line);
+  private static class WGSALine {
 
+    private final Map<String, String> toAdd;
+    private final String[] line;
+
+    /**
+     * @param toAdd
+     * @param line
+     */
+    private WGSALine(Map<String, String> toAdd, String[] line) {
+      super();
+      this.toAdd = toAdd;
+      this.line = line;
+    }
+
+  }
+
+  private static WGSALine processLine(Map<String, Integer> lookup, BufferedReader reader,
+                                      Set<String> interest, Logger log) throws IOException {
+    String[] line;
+
+    while ((line = reader.readLine().trim().split("\t")) != null) {
+      if (!line[lookup.get(TOPMED_FRZ6_AC)].equals(".")
+          && line[lookup.get(UNIQUE_VARIANT)].equals("Y")) break;
+    }
+
+    if (line == null) {
+      log.reportTimeWarning("Returning null WGSALine");
+      return null;
+    }
+
+    Map<String, String> toAdd = new HashMap<>();
     for (String key : interest) {
       toAdd.put(key, line[lookup.get(key)]);
     }
+    return new WGSALine(toAdd, line);
   }
 
-  private static void validate(Map<String, Integer> lookupIndel, VariantContext vc, String[] line) {
-    if (!vc.getContig().equals(line[lookupIndel.get(CHR)])) {
+  private static boolean validate(Map<String, Integer> lookupIndel, VariantContext vc,
+                                  String[] line) {
+    if (!vc.getContig().equals("chr" + line[lookupIndel.get(CHR)])) {
       throw new IllegalArgumentException("invalid contig , " + vc.getContig() + "\t"
                                          + line[lookupIndel.get(CHR)] + "\t"
                                          + vc.toStringWithoutGenotypes());
     }
     if (vc.getStart() != Integer.parseInt(line[lookupIndel.get(POS)])) {
-      throw new IllegalArgumentException("invalid position , " + vc.getStart() + "\t"
-                                         + line[lookupIndel.get(POS)] + "\t"
-                                         + vc.toStringWithoutGenotypes());
+      //      throw new IllegalArgumentException("invalid position , " + vc.getStart() + "\t"
+      //                                         + line[lookupIndel.get(POS)] + "\t"
+      //                                         + vc.toStringWithoutGenotypes());
+      return false;
     }
     if (!vc.getReference().getBaseString().equals(line[lookupIndel.get(REF)])) {
-      throw new IllegalArgumentException("invalid ref , " + vc.getReference() + "\t"
-                                         + line[lookupIndel.get(REF)] + "\t"
-                                         + vc.toStringWithoutGenotypes());
+      //      throw new IllegalArgumentException("invalid ref , " + vc.getReference() + "\t"
+      //                                         + line[lookupIndel.get(REF)] + "\t"
+      //                                         + vc.toStringWithoutGenotypes());
+      return false;
     }
     if (!vc.getAlternateAllele(0).getBaseString().equals(line[lookupIndel.get(ALT)])) {
-      throw new IllegalArgumentException("invalid alt , " + vc.getReference() + "\t"
-                                         + line[lookupIndel.get(ALT)] + "\t"
-                                         + vc.toStringWithoutGenotypes());
+      //      throw new IllegalArgumentException("invalid alt , " + vc.getReference() + "\t"
+      //                                         + line[lookupIndel.get(ALT)] + "\t"
+      //                                         + vc.toStringWithoutGenotypes());
+      return false;
     }
+    return true;
   }
 
   private static void addHeader(Set<String> newHeaderLines, VCFHeader header,
-                                VariantContextWriter writer) {
+                                VariantContextWriter writer, Logger log) {
 
     List<VCFInfoHeaderLine> vcfInfos = new ArrayList<>();
 
     for (String key : newHeaderLines) {
+      log.reportTimeInfo("adding header " + key);
       VCFInfoHeaderLine infoN = new VCFInfoHeaderLine(key, 1, VCFHeaderLineType.String,
                                                       key + " annotation transferred from WGSA");
       vcfInfos.add(infoN);
@@ -173,7 +259,7 @@ public class AddWGSA {
     c.addArg(CLI.ARG_OUTDIR, CLI.DESC_OUTDIR);
     c.addArg(WGSA_SNP_FILE, "corresponding wgsa snp annotation file");
     c.addArg(WGSA_INDEL_FILE, "corresponding wgsa indel annotation file");
-
+    c.parseWithExit(args);
     run(c.get(CLI.ARG_VCF), c.get(CLI.ARG_OUTDIR), c.get(WGSA_SNP_FILE), c.get(WGSA_INDEL_FILE));
   }
 
