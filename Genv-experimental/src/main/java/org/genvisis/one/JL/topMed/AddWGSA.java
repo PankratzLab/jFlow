c1da37b6941beb6edbc49453fe15a089f077ccdb
jlanej <jlanej@gmail.com>
2018-09-13 16:02:06 -0500

    AddWGSA: remove unused import

--- Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/AddWGSA.java
+++ Genv-experimental/src/main/java/org/genvisis/one/JL/topMed/AddWGSA.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
