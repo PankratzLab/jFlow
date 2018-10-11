e9f65127a5cc04705fda935c6e60fed334d41f31
Mark Hiner <hinerm@gmail.com>
2018-10-11 12:16:19 -0500

    BAFContamination
    Fix Marker import

--- Genvisis/src/main/java/org/genvisis/cnv/seq/qc/contamination/BAFContamination.java
+++ Genvisis/src/main/java/org/genvisis/cnv/seq/qc/contamination/BAFContamination.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import org.genvisis.cnv.filesys.ClusterFilterCollection;
+import org.genvisis.cnv.filesys.MarkerDetailSet.Marker;
 import org.genvisis.cnv.filesys.Project;
 import org.genvisis.cnv.filesys.Sample;
 import org.genvisis.cnv.hmm.PFB;
@@ -18,10 +19,9 @@ import org.pankratzlab.common.Logger;
 import org.pankratzlab.common.WorkerTrain;
 import org.pankratzlab.common.WorkerTrain.AbstractProducer;
 import org.pankratzlab.common.stats.LeastSquares;
-import org.pankratzlab.common.stats.RegressionModel;
 import org.pankratzlab.common.stats.LeastSquares.LS_TYPE;
+import org.pankratzlab.common.stats.RegressionModel;
 import com.google.common.primitives.Doubles;
-import ca.mcgill.mcb.pcingola.interval.Marker;
 
 public class BAFContamination {
 
