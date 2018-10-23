e6a6ca96eaf1c3632d6a468248e2f67c4f3c15f2
jlanej <jlanej@gmail.com>
2018-10-23 18:45:28 -0500

    VCFSimpleTally: Remove spurious javax.jms.IllegalStateException

--- SEQ/src/main/java/org/genvisis/seq/analysis/VCFSimpleTally.java
+++ SEQ/src/main/java/org/genvisis/seq/analysis/VCFSimpleTally.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
-import javax.jms.IllegalStateException;
 import org.genvisis.seq.analysis.VCFSimpleTally.GeneVariantPositionSummary.ADD_TYPE;
 import org.genvisis.seq.manage.GenotypeOps;
 import org.genvisis.seq.manage.VCFOps;
