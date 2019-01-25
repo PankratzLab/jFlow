cf1f1a7b0a7901a4cf0947f8ef0cb694f12b232c
cole0482 <cole0482@umn.edu>
2019-01-25 11:43:36 -0600

    Formatting

--- Genvisis/src/test/java/org/genvisis/cnv/filesys/TestLocusSet.java
+++ Genvisis/src/test/java/org/genvisis/cnv/filesys/TestLocusSet.java
@@ -12,40 +12,39 @@ import junit.framework.Assert;
 
 public class TestLocusSet {
 
-	/**
-	 * @see LocusSet#hasNoOverlap()
-	 */
-	@Test
-	public void testHasNoOverlap() {
-		LocusSet<Segment> testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
-																															new Segment((byte) 1, 15, 100)},
-																							 true, new Logger());
-		Assert.assertFalse("Overlap exists, expected hasNoOverlap() to return false",
-											 testSet.hasNoOverlap());
+  /**
+   * @see LocusSet#hasNoOverlap()
+   */
+  @Test
+  public void testHasNoOverlap() {
+    LocusSet<Segment> testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
+                                                              new Segment((byte) 1, 15, 100)},
+                                               true, new Logger());
+    Assert.assertFalse("Overlap exists, expected hasNoOverlap() to return false",
+                       testSet.hasNoOverlap());
 
-		testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
-																						new Segment((byte) 2, 15, 100)},
-														 true, new Logger());
-		Assert.assertTrue("Overlap does not exist, expected hasNoOverlap() to return true",
-											testSet.hasNoOverlap());
-	}
+    testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
+                                            new Segment((byte) 2, 15, 100)},
+                             true, new Logger());
+    Assert.assertTrue("Overlap does not exist, expected hasNoOverlap() to return true",
+                      testSet.hasNoOverlap());
+  }
 
-	/**
-	 * @see LocusSet#hasOverlap()
-	 */
-	@Test
-	public void testHasOverlap() {
-		LocusSet<Segment> testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
-																															new Segment((byte) 1, 15, 100)},
-																							 true, new Logger());
-		Assert.assertTrue("Overlap exists, expected hasOverlap() to return true",
-											testSet.hasOverlap());
+  /**
+   * @see LocusSet#hasOverlap()
+   */
+  @Test
+  public void testHasOverlap() {
+    LocusSet<Segment> testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
+                                                              new Segment((byte) 1, 15, 100)},
+                                               true, new Logger());
+    Assert.assertTrue("Overlap exists, expected hasOverlap() to return true", testSet.hasOverlap());
 
-		testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
-																						new Segment((byte) 2, 15, 100)},
-														 true, new Logger());
-		Assert.assertFalse("Overlap does not exist, expected hasOverlap() to return false",
-											 testSet.hasOverlap());
-	}
+    testSet = new LocusSet<>(new Segment[] {new Segment((byte) 1, 10, 1000),
+                                            new Segment((byte) 2, 15, 100)},
+                             true, new Logger());
+    Assert.assertFalse("Overlap does not exist, expected hasOverlap() to return false",
+                       testSet.hasOverlap());
+  }
 
 }
